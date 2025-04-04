8f61e97effafe5dbc66263447f186888de633657
DaeYoung0726 <pdy000726@naver.com>
2025-04-05 00:10:34 +0900

    chore: create project

---
+++ src/main/java/kr/co/yeogiga/YeogigaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yeogiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogigaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeogigaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/yeogiga/YeogigaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yeogiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogigaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
