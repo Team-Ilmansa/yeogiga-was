f5ca87748657c4979aee53a754eedc1abe97ccaa
박대영 <125343432+daeyoung0726@users.noreply.github.com>
2025-04-10 16:42:08 +0900

    Feature: 문서화 관련 설정 (#4)
    * feat: docs 의존성 추가
    
    * chore: docs 디스크 저장 위치 지정
    
    * feat: Swagger Docs 설정
    
    * feat: Api-Link-Checker Docs 설정
    
    * docs: build.gradle 의존성 구분

---
+++ src/main/java/kr/co/yeogiga/infrastructure/config/ApiLinkCheckerConfig.java
@@ -0,0 +1,9 @@
+package kr.co.yeogiga.infrastructure.config;
+
+import api.link.checker.annotation.EnableApiLinkChecker;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableApiLinkChecker
+public class ApiLinkCheckerConfig {
+}
