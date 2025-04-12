b247e70f4883a1542253a2d7ba8a72548522e333
hky035 <hky035@gmail.com>
2025-04-12 19:36:02 +0900

    test: 사용자 생성 로직 테스트

--- src/test/java/kr/co/yeogiga/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/yeogiga/domain/user/repository/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import kr.co.yeogiga.domain.oauth.entity.OAuth;
 import kr.co.yeogiga.domain.oauth.repository.OAuthRepository;
 import kr.co.yeogiga.domain.user.entity.User;
 import kr.co.yeogiga.domain.oauth.type.Platform;
+import kr.co.yeogiga.domain.user.type.Role;
 import kr.co.yeogiga.infrastructure.config.JpaConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,6 +38,7 @@ public class UserRepositoryTest {
                 .username("tester")
                 .nickname("test")
                 .password("testpw")
+                .role(Role.USER)
                 .build();
 
         // when
@@ -53,6 +55,7 @@ public class UserRepositoryTest {
         User relatedUser = User.builder()
                 .email("fromPlatformt@test.com")
                 .nickname("tempNickname")
+                .role(Role.USER)
                 .build();
 
         OAuth oauth = OAuth.builder()
