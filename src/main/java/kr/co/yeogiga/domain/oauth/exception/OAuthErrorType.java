6d274c1498846b074e8da4b45af0dc4b497b7331
hky035 <hky035@gmail.com>
2025-04-14 23:53:59 +0900

    feat: OAuth 관련 에러 코드 추가 - 미지원 소셜 로그인 플랫폼

--- src/main/java/kr/co/yeogiga/domain/oauth/exception/OAuthErrorType.java
+++ src/main/java/kr/co/yeogiga/domain/oauth/exception/OAuthErrorType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OAuthErrorType implements BaseErrorType {
 
-    OAUTH_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "O001", "소셜 로그인 과정에서 에러가 발생하였습니다. 관리자에게 문의하세요.");
+    OAUTH_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "O001", "소셜 로그인 과정에서 에러가 발생하였습니다. 관리자에게 문의하세요."),
+    UNSUPPORTED_PLATFORM(HttpStatus.BAD_REQUEST, "O0002", "지원하지 않는 소셜 로그인 플랫폼입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
