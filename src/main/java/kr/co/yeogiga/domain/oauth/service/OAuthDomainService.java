e08ce87d7cac508e4046076be24bec2a9d204a97
hky035 <hky035@gmail.com>
2025-04-15 00:00:01 +0900

    feat: OAuthDomainService 클래스 생성

---
+++ src/main/java/kr/co/yeogiga/domain/oauth/service/OAuthDomainService.java
@@ -0,0 +1,16 @@
+package kr.co.yeogiga.domain.oauth.service;
+
+import kr.co.yeogiga.domain.oauth.entity.OAuth;
+import kr.co.yeogiga.domain.oauth.repository.OAuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthDomainService {
+    private final OAuthRepository oauthRepository;
+
+    public OAuth save(OAuth oauth) {
+        return oauthRepository.save(oauth);
+    }
+}
