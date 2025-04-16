ac2ad771f40d33fd844e6f03835bb7989f711756
hky035 <hky035@gmail.com>
2025-04-16 22:26:50 +0900

    feat: 로그인 dto 통합

--- src/main/java/kr/co/yeogiga/application/auth/dto/SignInDto.java
+++ src/main/java/kr/co/yeogiga/application/auth/dto/SignInDto.java
@@ -1,8 +1,19 @@
 package kr.co.yeogiga.application.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 public class SignInDto {
+
+    @Builder
+    @Schema(name = "OAuthRequest", description = "OAuth 로그인 요청 DTO")
+    public record OAuthRequest(
+            @Schema(description = "OAuth 리소스 서버에서 발급받은 인증 코드", example = "abcd123")
+            @NotBlank(message = "인증 코드값은 필수입니다.")
+            String code
+    ) {
+    }
 
     @Builder
     public record Response(
