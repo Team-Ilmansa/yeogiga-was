8e4f4ea77fa470de960201aee9d28f6b7ed11df0
hky035 <rldud035@naver.com>
2025-04-16 20:52:24 +0900

    feat: jwt 토큰 파싱 클래스 생성

---
+++ src/main/java/kr/co/yeogiga/common/jwt/TokenParser.java
@@ -0,0 +1,31 @@
+package kr.co.yeogiga.common.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoder;
+import kr.co.yeogiga.common.jwt.base64.CustomBase64UrlDecoder;
+import kr.co.yeogiga.infrastructure.properties.JwtProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Component
+public class TokenParser {
+    private final JwtParser jwtParser;
+
+    @Autowired
+    public TokenParser(JwtProperties jwtProperties) {
+        Decoder<InputStream, InputStream> base54UrlDecoder = new CustomBase64UrlDecoder();
+        this.jwtParser = Jwts.parser()
+                .verifyWith(jwtProperties.getSecretKey())
+                .b64Url(base54UrlDecoder)
+                .build();
+    }
+
+    public Claims parseClaims(String token) {
+        return jwtParser.parseSignedClaims(token)
+                .getPayload();
+    }
+}
