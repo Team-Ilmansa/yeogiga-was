5102c3cc15ecf592b27ab1e684d1be3b58fa9b89
hky035 <hky035@gmail.com>
2025-04-16 22:29:46 +0900

    feat: OAuthController swagger 문서화

---
+++ src/main/java/kr/co/yeogiga/presentation/auth/api/OAuthApi.java
@@ -0,0 +1,70 @@
+package kr.co.yeogiga.presentation.auth.api;
+
+import api.link.checker.annotation.ApiGroup;
+import api.link.checker.annotation.TrackApi;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import kr.co.yeogiga.application.auth.dto.SignInDto;
+import kr.co.yeogiga.domain.oauth.type.OAuthPlatform;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@ApiGroup(value = "[OAuth2 인증 API]")
+@Tag(name = "[OAuth2 인증 API]", description = "OAuth2 인증 관련 API")
+public interface OAuthApi {
+
+    @TrackApi(description = "OAuth2 로그인")
+    @Operation(summary = "OAuth2 로그인", description = "OAuth2를 통한 로그인 API 입니다. 처음 로그인하는 사용자는 '여기가'에 유저 정보를 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(name = "기존 회원가입된 사용자", value = """
+                                        {
+                                            "code": 200,
+                                            "message": "요청이 성공하였습니다.",
+                                            "data": {
+                                                "token": {
+                                                    "accessToken": "xxxxx.xxxxx.xxxxx",
+                                                    "refreshToken": "xxxxx.xxxxx.xxxxx"
+                                                },
+                                                "shouldSignup": false
+                                            }
+                                        }
+                                    """),
+                            @ExampleObject(name = "첫 로그인 사용자", value = """
+                                        {
+                                             "code": 200,
+                                             "message": "요청이 성공하였습니다.",
+                                             "data": {
+                                                 "token": null,
+                                                 "shouldSignup": true
+                                             }
+                                        }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "400", description = "유효성 검사 실패",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(name = "인증 코드 유효성 검사 실패", value = """
+                                        {
+                                            "code": "G002",
+                                            "errors": {
+                                                "code": "인증 코드값은 필수입니다."
+                                            }
+                                        }
+                                    """),
+                            @ExampleObject(name = "플랫폼 유효성 검사 실패", value = """
+                                        {
+                                            "code": "G003",
+                                            "message": "지원하지 않는 Path Variable 값입니다."
+                                        }
+                                    """)
+                    }))
+    })
+    ResponseEntity<?> signUp(@PathVariable OAuthPlatform platform, @Valid @RequestBody SignInDto.OAuthRequest request);
+}
--- src/main/java/kr/co/yeogiga/presentation/auth/controller/OAuthController.java
+++ src/main/java/kr/co/yeogiga/presentation/auth/controller/OAuthController.java
@@ -1,26 +1,28 @@
 package kr.co.yeogiga.presentation.auth.controller;
 
-import kr.co.yeogiga.application.auth.dto.OAuthSignInDto;
+import jakarta.validation.Valid;
+import kr.co.yeogiga.application.auth.dto.SignInDto;
 import kr.co.yeogiga.application.auth.service.OAuthManagementService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.domain.oauth.type.OAuthPlatform;
+import kr.co.yeogiga.presentation.auth.api.OAuthApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth/oauth")
-public class OAuthController {
+public class OAuthController implements OAuthApi {
     private final OAuthManagementService oAuthManagementService;
 
     @PostMapping("/sign-in/{platform}")
-    public ResponseEntity<?> signUp(@PathVariable OAuthPlatform platform, @RequestBody OAuthSignInDto.Request request) {
+    public ResponseEntity<?> signUp(@PathVariable OAuthPlatform platform, @Valid @RequestBody SignInDto.OAuthRequest request) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.from(oAuthManagementService.signIn(platform, request.code())));
     }
