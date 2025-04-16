3ab47d2067a6d67017346e61d4efc5cbbf1a44bd
DaeYoung0726 <pdy000726@naver.com>
2025-04-16 13:33:41 +0900

    refact: 이미지 메타데이터 처리 서비스 SRP 처리

--- src/main/java/kr/co/yeogiga/application/image/service/ImageMetadataService.java
+++ src/main/java/kr/co/yeogiga/application/image/service/ImageMetadataService.java
@@ -5,13 +5,11 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.GpsDirectory;
-import kr.co.yeogiga.application.image.dto.ImageUploadRequest;
+import kr.co.yeogiga.application.image.dto.ImageMetadataDto;
 import kr.co.yeogiga.common.util.DateTimeUtils;
-import kr.co.yeogiga.infrastructure.s3.AwsS3Storage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
@@ -23,21 +21,21 @@ import java.util.Date;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ImageUploadAndMetadataService {
-    private final AwsS3Storage awsS3Storage;
+public class ImageMetadataService {
 
     /**
-     * 이미지 업로드 및 메타데이터 추출
+     * 이미지 메타데이터 추출
      *
-     * @param imageUploadRequest : 업로드 이미지에 대한 정보 및 바이너리 데이터
+     * @param bytes            : 이미지의 바이너리 데이터
+     * @param originalFilename : 이미지의 이름
+     * @return (위도, 경도, 시간) 정보
      */
-    @Async
-    public void uploadAndExtractMetadata(ImageUploadRequest imageUploadRequest) {
+    public ImageMetadataDto extractMetadata(byte[] bytes, String originalFilename) {
         Double latitude = null;
         Double longitude = null;
         LocalDateTime date = null;
 
-        try (InputStream stream = new ByteArrayInputStream(imageUploadRequest.bytes())) {
+        try (InputStream stream = new ByteArrayInputStream(bytes)) {
             Metadata metadata = ImageMetadataReader.readMetadata(stream);
 
             Pair<Double, Double> location = extractGeoLocation(metadata);
@@ -47,14 +45,10 @@ public class ImageUploadAndMetadataService {
             date = extractTakenDate(metadata);
 
         } catch (IOException | ImageProcessingException e) {
-            log.error("Failed to upload image and extract metadata - filename: {}", imageUploadRequest.originalFilename(), e);
+            log.error("Failed to upload image and extract metadata - filename: {}", originalFilename, e);
         }
 
-        String url = awsS3Storage.upload(imageUploadRequest);
-        log.info("위치: " + latitude + ", " + longitude);
-        log.info("시간: " + date);
-        log.info("이미지 경로: " + url);
-
+        return ImageMetadataDto.of(latitude, longitude, date);
     }
 
     /**
