a530f232c333c5c01e07cfef534250bdac946f8b
DaeYoung0726 <pdy000726@naver.com>
2025-04-17 02:17:21 +0900

    feat: 이미지 삭제 로직 rest api 연결

--- src/main/java/kr/co/yeogiga/presentation/image/controller/ImageController.java
+++ src/main/java/kr/co/yeogiga/presentation/image/controller/ImageController.java
@@ -1,9 +1,15 @@
 package kr.co.yeogiga.presentation.image.controller;
 
+import kr.co.yeogiga.application.image.dto.ImageUrlDto;
+import kr.co.yeogiga.application.image.service.ImageDeleteProcessor;
 import kr.co.yeogiga.application.image.service.ImageUploadProcessor;
+import kr.co.yeogiga.common.response.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +22,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageUploadProcessor imageUploadProcessor;
+    private final ImageDeleteProcessor imageDeleteProcessor;
 
     @PostMapping("/{tripId}/images")
-    public void uploadImages(@RequestPart(value = "images", required = false) List<MultipartFile> images,
-                             @PathVariable Long tripId) {
+    public ResponseEntity<?> uploadImages(@RequestPart(value = "images", required = false) List<MultipartFile> images,
+                                          @PathVariable Long tripId) {
         imageUploadProcessor.process(images, tripId);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+    @DeleteMapping("/{tripId}/images")
+    public ResponseEntity<?> deleteImage(@RequestBody ImageUrlDto imageUrlDto) {
+        imageDeleteProcessor.process(imageUrlDto);
+        return ResponseEntity.ok(SuccessResponse.ok());
     }
 }
