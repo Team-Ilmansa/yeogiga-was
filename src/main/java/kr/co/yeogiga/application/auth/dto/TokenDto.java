9b19f79ffb19f145c289ecb837d5cdb2540a0ff6
hky035 <rldud035@naver.com>
2025-04-16 20:53:54 +0900

    feat: jwt 토큰 dto 클래스 작성

---
+++ src/main/java/kr/co/yeogiga/application/auth/dto/TokenDto.java
@@ -0,0 +1,11 @@
+package kr.co.yeogiga.application.auth.dto;
+
+
+public record TokenDto (
+    String accessToken,
+    String refreshToken
+) {
+    public static TokenDto of(String accessToken, String refreshToken) {
+        return new TokenDto(accessToken, refreshToken);
+    }
+}
