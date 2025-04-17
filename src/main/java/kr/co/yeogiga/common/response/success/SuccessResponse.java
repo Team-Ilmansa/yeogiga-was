aab0eb5e83f05f5eb0426af8c2745a632b76d544
DaeYoung0726 <pdy000726@naver.com>
2025-04-17 11:42:12 +0900

    refact: SuccessType 삭제

--- src/main/java/kr/co/yeogiga/common/response/success/SuccessResponse.java
+++ src/main/java/kr/co/yeogiga/common/response/success/SuccessResponse.java
@@ -1,7 +1,6 @@
 package kr.co.yeogiga.common.response.success;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import kr.co.yeogiga.common.response.success.type.SuccessType;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
 
@@ -19,25 +18,10 @@ public record SuccessResponse<T>(
                 .build();
     }
 
-    public static SuccessResponse<?> from(SuccessType success) {
-        return SuccessResponse.builder()
-                .code(success.getStatusCode())
-                .message(success.getMessage())
-                .build();
-    }
-
     public static <T> SuccessResponse<?> from(T data) {
         return SuccessResponse.builder()
                 .code(HttpStatus.OK.value())
                 .message("요청이 성공하였습니다.")
-                .data(data)
-                .build();
-    }
-
-    public static <T> SuccessResponse<?> of(SuccessType success, T data) {
-        return SuccessResponse.builder()
-                .code(success.getStatusCode())
-                .message(success.getMessage())
                 .data(data)
                 .build();
     }
