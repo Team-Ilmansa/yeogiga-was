5c7328e2e829580bf56e9c8a40f3ea4655507a63
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 18:02:50 +0900

    feat: TripMember Entity 생성

---
+++ src/main/java/kr/co/yeogiga/domain/trip/entity/TripMember.java
@@ -0,0 +1,37 @@
+package kr.co.yeogiga.domain.trip.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.yeogiga.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "trip_member")
+public class TripMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trip_id")
+    private Trip trip;
+
+    @Builder
+    public TripMember(User user, Trip trip) {
+        this.user = user;
+        this.trip = trip;
+    }
+}
