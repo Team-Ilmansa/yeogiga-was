e168d8c932adc8535eadaf1d53603a14755d39aa
hky035 <hky035@gmail.com>
2025-04-27 02:43:04 +0900

    feat: 토큰 인증 관련 에러 코드 위치 변경
     * CommonErrorType -> AuthErrorType

--- src/main/java/kr/co/yeogiga/common/response/error/type/CommonErrorType.java
+++ src/main/java/kr/co/yeogiga/common/response/error/type/CommonErrorType.java
@@ -11,11 +11,6 @@ public enum CommonErrorType implements BaseErrorType {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G001", "서버 내부 에러입니다."),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G002", "유효성 검증에 실패하였습니다."),
     PATH_VARIABLE_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G003", "지원하지 않는 Path Variable 값입니다."),
-
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "G004", "만료된 토큰입니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "G005", "잘못된 토큰입니다."),
-    INVALID_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "G006", "토큰 서명이 잘못되었습니다."),
-    UNKNOWN_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "G007", "토큰 인증 과정 중 에러가 발생하였습니다."),
     ;
 
     private final HttpStatus status;
