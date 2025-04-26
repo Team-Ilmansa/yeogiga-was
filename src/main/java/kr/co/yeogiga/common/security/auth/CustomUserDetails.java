55db9437dc9bc9fce5b3287b07c106f613da4c03
hky035 <hky035@gmail.com>
2025-04-27 01:51:45 +0900

    feat: UserDetails 및 UserDetailsService 구현

---
+++ src/main/java/kr/co/yeogiga/common/security/auth/CustomUserDetails.java
@@ -0,0 +1,8 @@
+package kr.co.yeogiga.common.security.auth;
+
+import kr.co.yeogiga.domain.user.type.Role;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface CustomUserDetails extends UserDetails {
+    public Role getRole();
+}
