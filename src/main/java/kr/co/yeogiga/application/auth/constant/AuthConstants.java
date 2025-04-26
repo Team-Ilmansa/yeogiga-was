bb1204a3adee90df4a6f7979daeb7d1675c6c8fc
hky035 <hky035@gmail.com>
2025-04-27 01:38:52 +0900

    feat: Token Type 상수 추가

--- src/main/java/kr/co/yeogiga/application/auth/constant/AuthConstants.java
+++ src/main/java/kr/co/yeogiga/application/auth/constant/AuthConstants.java
@@ -1,7 +1,8 @@
 package kr.co.yeogiga.application.auth.constant;
 
 public final class AuthConstants {
-    private AuthConstants() {}
+    private AuthConstants() { }
 
+    public static final String TOKEN_TYPE = "Bearer";
     public static final String REFRESH_TOKEN_PREFIX = "refreshToken";
 }
