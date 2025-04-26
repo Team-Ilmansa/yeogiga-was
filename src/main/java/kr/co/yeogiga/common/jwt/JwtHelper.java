870658de999917962bf3729591d8b9c2522bb911
hky035 <hky035@gmail.com>
2025-04-27 01:49:04 +0900

    feat: 토큰 값 추출을 위한 정적 메서드 추가

--- src/main/java/kr/co/yeogiga/common/jwt/JwtHelper.java
+++ src/main/java/kr/co/yeogiga/common/jwt/JwtHelper.java
@@ -8,12 +8,18 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+import static kr.co.yeogiga.application.auth.constant.AuthConstants.TOKEN_TYPE;
+
 @Component
 @RequiredArgsConstructor
 public class JwtHelper {
     private final TokenBuilder tokenBuilder;
     private final TokenParser tokenParser;
     private final JwtProperties jwtProperties;
+
+    public static String resolveToken(String token) {
+        return token.replace(TOKEN_TYPE, "").trim();
+    }
 
     public String generateAccessToken(String username, String nickname, Long userId) {
         return tokenBuilder.build(username, generateClaims(nickname, userId), jwtProperties.getAccessTokenExpiration());
