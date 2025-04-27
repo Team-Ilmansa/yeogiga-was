cc3c9a9f930739b0d426b71c61af2ea1373d666d
DaeYoung0726 <pdy000726@naver.com>
2025-04-28 01:41:45 +0900

    feat: 여행 생성 단계 목적지 CRUD 로직 구현

---
+++ src/main/java/kr/co/yeogiga/application/trip/service/TripPlaceEditingService.java
@@ -0,0 +1,122 @@
+package kr.co.yeogiga.application.trip.service;
+
+import kr.co.yeogiga.application.trip.dto.TripPlaceDto;
+import kr.co.yeogiga.common.exception.CustomException;
+import kr.co.yeogiga.domain.trip.exception.TripErrorType;
+import kr.co.yeogiga.infrastructure.redis.RedisRepository;
+import kr.co.yeogiga.infrastructure.redis.constant.PlaceConstant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 여행 생성 단계에서 사용자들이 선택한 "목적지"들을 임시 저장하고 관리하는 서비스 클래스.
+ * - 아직 확정되지 않은 여행 목적지 편집(추가, 삭제, 수정, 조회) 수행
+ * - Redis를 임시 저장소로 사용
+ */
+@Service
+@RequiredArgsConstructor
+public class TripPlaceEditingService {
+    private final RedisRepository redisRepository;
+
+    /**
+     * 특정 여행(tripId)과 일차(day)에 저장된 목적지 리스트를 조회하는 메서드
+     *
+     * @param tripId : 여행 ID
+     * @param day    : 여행 일차 (1일차, 2일차 등)
+     * @return : 저장된 TripPlaceDto.StoredFormat 리스트
+     */
+    public List<TripPlaceDto.StoredFormat> getPlacesInEditing(Long tripId, int day) {
+        String listKey = PlaceConstant.listKey(tripId, day);
+        return redisRepository.getList(listKey, TripPlaceDto.StoredFormat.class);
+    }
+
+    /**
+     * 편집 중인 여행 일정에 새로운 목적지를 추가하는 메서드
+     * - Set을 통해 이미 추가된 장소인지 확인
+     * - 중복이 없다면 List & Set에 추가 (저장 포맷에 맞게 변환 후 저장)
+     *
+     * @param tripId : 여행 ID
+     * @param day    : 여행 일차
+     * @param place  : 추가할 TripPlaceDto.Request 객체
+     * @throws CustomException ALREADY_ADDED_PLACE : 이미 목적지를 추가한 경우
+     */
+    public void addPlaceInEditing(Long tripId, int day, TripPlaceDto.Request place) {
+        String listKey = PlaceConstant.listKey(tripId, day);
+        String setKey = PlaceConstant.setKey(tripId, day);
+
+        String placeUniqueKey = makeUniqueKey(place.name(), place.latitude(), place.longitude());
+        if (redisRepository.existsInSet(setKey, placeUniqueKey)) {
+            throw new CustomException(TripErrorType.ALREADY_ADDED_PLACE);
+        }
+
+        redisRepository.setList(listKey, place.toStoredFormat());
+        redisRepository.addToSet(setKey, placeUniqueKey);
+    }
+
+    /**
+     * 편집 중인 여행 일정에서 특정 목적지를 삭제하는 메서드
+     * - List에서 id에 해당하는 목적지 조회
+     * - 찾은 경우 List & Set에 삭제
+     *
+     * @param tripId  : 여행 ID
+     * @param day     : 여행 일차
+     * @param placeId : 삭제할 목적지 ID
+     */
+    public void deletePlaceInEditing(Long tripId, int day, String placeId) {
+        String listKey = PlaceConstant.listKey(tripId, day);
+        String setKey = PlaceConstant.setKey(tripId, day);
+
+        List<TripPlaceDto.StoredFormat> places = redisRepository.getList(listKey, TripPlaceDto.StoredFormat.class);
+
+        TripPlaceDto.StoredFormat target = places.stream()
+                .filter(p -> placeId.equals(p.id()))
+                .findFirst()
+                .orElse(null);
+
+        if (target == null) {
+            return;
+        }
+
+        redisRepository.removeFromList(listKey, target);
+
+        String placeUniqueKey = makeUniqueKey(target.name(), target.latitude(), target.longitude());
+        redisRepository.removeFromSet(setKey, placeUniqueKey);
+    }
+
+    /**
+     * 편집 중인 여행 일정의 목적지를 수정하는 메서드 (목적지의 새로운 순서로 덮어쓰기 등)
+     * - 기존 List & Set 모두 삭제
+     * - 새로운 순서대로 List & Set에 저장
+     *
+     * @param tripId : 여행 ID
+     * @param day    : 여행 일차
+     * @param places : 새로운 순서의 TripPlaceDto.Request 리스트
+     */
+    public void updatePlacesInEditing(Long tripId, int day, List<TripPlaceDto.Request> places) {
+        String listKey = PlaceConstant.listKey(tripId, day);
+        String setKey = PlaceConstant.setKey(tripId, day);
+
+        redisRepository.del(listKey);
+        redisRepository.del(setKey);
+
+        for (TripPlaceDto.Request place : places) {
+            redisRepository.setList(listKey, place.toStoredFormat());
+            String placeUniqueKey = makeUniqueKey(place.name(), place.latitude(), place.longitude());
+            redisRepository.addToSet(setKey, placeUniqueKey);
+        }
+    }
+
+    /**
+     * 목적지의 고유 식별 키를 생성하는 메서드 (장소명 + 위도 + 경도 조합)
+     *
+     * @param name      : 목적지 이름
+     * @param latitude  : 목적지 위도
+     * @param longitude : 목적지 경도
+     * @return : unique key
+     */
+    private String makeUniqueKey(String name, double latitude, double longitude) {
+        return name + ":" + latitude + ":" + longitude;
+    }
+}
