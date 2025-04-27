da1dbc91fca19286c5286815d970a3d2ae9e92a9
DaeYoung0726 <pdy000726@naver.com>
2025-04-28 01:42:10 +0900

    feat: 여행 생성 단계 목적지 CRUD rest api 연결

---
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripPlaceEditingController.java
@@ -0,0 +1,59 @@
+package kr.co.yeogiga.presentation.trip.controller;
+
+import kr.co.yeogiga.application.trip.dto.TripPlaceDto;
+import kr.co.yeogiga.application.trip.service.TripPlaceEditingService;
+import kr.co.yeogiga.common.response.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/trip")
+@RequiredArgsConstructor
+public class TripPlaceEditingController {
+    private final TripPlaceEditingService tripPlaceEditingService;
+
+    @PostMapping("/{tripId}/days/{day}/places")
+    public ResponseEntity<?> addPlace(@PathVariable Long tripId,
+                                      @PathVariable int day,
+                                      @RequestBody TripPlaceDto.Request request) {
+
+        tripPlaceEditingService.addPlaceInEditing(tripId, day, request);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+
+    @GetMapping("/{tripId}/days/{day}/places")
+    public ResponseEntity<?> getPlaces(@PathVariable Long tripId, @PathVariable int day) {
+        return ResponseEntity.ok(
+                SuccessResponse.from(tripPlaceEditingService.getPlacesInEditing(tripId, day))
+        );
+    }
+
+    @PutMapping("/{tripId}/days/{day}/places")
+    public ResponseEntity<?> updatePlaces(@PathVariable Long tripId,
+                                          @PathVariable int day,
+                                          @RequestBody List<TripPlaceDto.Request> requests) {
+
+        tripPlaceEditingService.updatePlacesInEditing(tripId, day, requests);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+    @DeleteMapping("/{tripId}/days/{day}/places/{placeId}")
+    public ResponseEntity<?> deletePlace(@PathVariable Long tripId,
+                                         @PathVariable int day,
+                                         @PathVariable String placeId) {
+
+        tripPlaceEditingService.deletePlaceInEditing(tripId, day, placeId);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+}
