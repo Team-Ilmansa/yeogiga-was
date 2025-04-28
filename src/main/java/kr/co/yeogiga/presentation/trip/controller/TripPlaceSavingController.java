abee9a8ea1b6503cb8e9d9a18dce8b498d7d88fd
DaeYoung0726 <pdy000726@naver.com>
2025-04-28 22:19:28 +0900

    feat: 여행 목적지 확정 rest api 연결

---
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripPlaceSavingController.java
@@ -0,0 +1,28 @@
+package kr.co.yeogiga.presentation.trip.controller;
+
+import kr.co.yeogiga.application.trip.dto.TripPlaceDto;
+import kr.co.yeogiga.application.trip.service.TripPlaceSavingService;
+import kr.co.yeogiga.common.response.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/trip")
+@RequiredArgsConstructor
+public class TripPlaceSavingController {
+    private final TripPlaceSavingService tripPlaceSavingService;
+
+    @PostMapping("/{tripId}/complete")
+    public ResponseEntity<?> completeTrip(@PathVariable Long tripId,
+                                          @RequestBody TripPlaceDto.CompleteRequest request) {
+
+        tripPlaceSavingService.completeTrip(tripId, request.lastDay());
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.ok());
+    }
+}
