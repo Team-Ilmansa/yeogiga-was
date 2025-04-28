7adb55d427b983f1f47986c8c438132416928949
hky035 <hky035@gmail.com>
2025-04-29 02:48:14 +0900

    feat: 토큰 재발급 및 로그아웃 엔드포인트 추가

--- src/main/java/kr/co/yeogiga/infrastructure/config/security/constant/EndpointConstants.java
+++ src/main/java/kr/co/yeogiga/infrastructure/config/security/constant/EndpointConstants.java
@@ -6,10 +6,11 @@ public final class EndpointConstants {
     public static final String[] PUBLIC_ENDPOINTS = {
             "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**",
             "/webjars/**", "/error", "/api-checker/**", "/v1/api/link/checker/**",
-            "/api/v1/auth/**"
+            "/api/v1/auth/oauth/**", "/api/v1/auth/reissue",
     };
 
     public static final String[] USER_ENDPOINTS = {
-            "/api/v1/trip/**"
+            "/api/v1/trip/**",
+            "/api/v1/auth/sign-out"
     };
 }
