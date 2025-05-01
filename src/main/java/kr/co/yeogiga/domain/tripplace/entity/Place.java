fc052211a3f7ee34907bc83de6dc26255e8cbbc3
DaeYoung0726 <pdy000726@naver.com>
2025-05-01 16:13:41 +0900

    refact: 불필요 필드 제거

--- src/main/java/kr/co/yeogiga/domain/tripplace/entity/Place.java
+++ src/main/java/kr/co/yeogiga/domain/tripplace/entity/Place.java
@@ -3,8 +3,6 @@ package kr.co.yeogiga.domain.tripplace.entity;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class Place {
     private String id;
@@ -13,18 +11,16 @@ public class Place {
     private double longitude;
     private String placeType;
     private double order;
-    private LocalDateTime visitedAt;
 
     @Builder
-    public Place(String id, String name, double latitude, double longitude,
-                 String placeType, double order, LocalDateTime visitedAt) {
+    public Place(String id, String name, double latitude,
+                 double longitude, String placeType, double order) {
         this.id = id;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
         this.placeType = placeType;
         this.order = order;
-        this.visitedAt = visitedAt;
     }
 
     public void updateOrder(double order) {
