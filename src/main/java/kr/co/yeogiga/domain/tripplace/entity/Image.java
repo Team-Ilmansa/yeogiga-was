e79dbc11aafc01273d5301f22527ad839d7a10aa
DaeYoung0726 <pdy000726@naver.com>
2025-05-01 18:22:02 +0900

    feat: 이미지 객체 생성

---
+++ src/main/java/kr/co/yeogiga/domain/tripplace/entity/Image.java
@@ -0,0 +1,25 @@
+package kr.co.yeogiga.domain.tripplace.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class Image {
+    private String id;
+    private String url;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime date;
+
+    @Builder
+    public Image(String url, Double latitude, Double longitude, LocalDateTime date) {
+        this.id = UUID.randomUUID().toString();
+        this.url = url;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.date = date;
+    }
+}
