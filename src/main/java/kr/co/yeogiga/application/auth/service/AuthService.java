b351896e8d706083f7db6fd90a670328e2400e96
hky035 <hky035@gmail.com>
2025-05-03 00:10:25 +0900

    docs: 주석 통일성 유지를 위한 변경

--- src/main/java/kr/co/yeogiga/application/auth/service/AuthService.java
+++ src/main/java/kr/co/yeogiga/application/auth/service/AuthService.java
@@ -44,7 +44,7 @@ public class AuthService {
      *
      * @param request               로그인 요청 dto(username, password)
      * @throws CustomException      AuthErrorType.AUTHENTICATION_FAIL 아이디 및 비밀번호 불일치
-     * @return                      토큰(access token, refresh token)
+     * @return                      토큰(accessToken, refreshToken)
      */
     public TokenDto signIn(SignInDto.Request request) {
         User user = userService.readByUsername(request.username())
