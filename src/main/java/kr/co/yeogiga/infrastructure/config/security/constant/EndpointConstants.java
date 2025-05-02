2a4b942492ed55fa25eaaa6ffa2b2158df3f565b
hky035 <hky035@gmail.com>
2025-05-03 01:12:29 +0900

    feat: 인증 관련 엔드포인트 퍼블릭 권한 통합

--- src/main/java/kr/co/yeogiga/infrastructure/config/security/constant/EndpointConstants.java
+++ src/main/java/kr/co/yeogiga/infrastructure/config/security/constant/EndpointConstants.java
@@ -6,11 +6,10 @@ public final class EndpointConstants {
     public static final String[] PUBLIC_ENDPOINTS = {
             "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**",
             "/webjars/**", "/error", "/api-checker/**", "/v1/api/link/checker/**",
-            "/api/v1/auth/oauth/**", "/api/v1/auth/reissue", "/api/v1/auth/sign-in", "/api/v1/auth/sign-up"
+            "/api/v1/auth/**"
     };
 
     public static final String[] USER_ENDPOINTS = {
             "/api/v1/trip/**",
-            "/api/v1/auth/sign-out"
     };
 }
