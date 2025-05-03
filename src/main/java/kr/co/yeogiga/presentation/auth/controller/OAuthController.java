4a2a63a79573f866ae6149ed786bb1da90717192
hky035 <hky035@gmail.com>
2025-05-03 16:57:37 +0900

    feat: 게스트 사용자 회원 등록 api 추가

--- src/main/java/kr/co/yeogiga/presentation/auth/controller/OAuthController.java
+++ src/main/java/kr/co/yeogiga/presentation/auth/controller/OAuthController.java
@@ -2,17 +2,21 @@ package kr.co.yeogiga.presentation.auth.controller;
 
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.auth.dto.SignInDto;
+import kr.co.yeogiga.application.auth.dto.SignUpDto;
 import kr.co.yeogiga.application.auth.service.OAuthManagementService;
 import kr.co.yeogiga.application.auth.type.Device;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
+import kr.co.yeogiga.common.security.auth.CustomUserDetails;
 import kr.co.yeogiga.common.util.CookieUtil;
 import kr.co.yeogiga.domain.oauth.type.OAuthPlatform;
 import kr.co.yeogiga.presentation.auth.api.OAuthApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +38,15 @@ public class OAuthController implements OAuthApi {
             @PathVariable(name = "platform") OAuthPlatform platform,
             @Valid @RequestBody SignInDto.OAuthRequest request) {
         return createSignInResponse(device, oAuthManagementService.signIn(platform, request.code()));
+    }
+
+
+    @PutMapping("/register")
+    public ResponseEntity<?> register(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody SignUpDto.Register request) {
+        oAuthManagementService.register(userDetails.getUserId(), request);
+        return ResponseEntity.ok().build();
     }
 
     private ResponseEntity<?> createSignInResponse(Device device, SignInDto.Response response) {
