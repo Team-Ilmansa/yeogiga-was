31d4b767de2b4b6addda76974d6d38d08f24415a
DaeYoung0726 <pdy000726@naver.com>
2025-05-04 15:32:44 +0900

    refact: 역할 식별을 위한 변수 및 메서드명 변경

--- src/main/java/kr/co/yeogiga/infrastructure/redis/constant/PlaceConstant.java
+++ src/main/java/kr/co/yeogiga/infrastructure/redis/constant/PlaceConstant.java
@@ -4,16 +4,16 @@ public final class PlaceConstant {
 
     private PlaceConstant() { }
 
-    private static final String LIST_KEY_FORMAT = "trip:%d:day:%d:places";
-    private static final String SET_KEY_FORMAT = "trip:%d:day:%d:places:set";
+    private static final String DAY_PLACES_KEY_FORMAT = "trip:%d:day:%d:places";
+    private static final String DAY_PLACE_SET_KEY_FORMAT = "trip:%d:day:%d:places:set";
     private static final String TEMP_LIST_KEY_FORMAT = "trip:%d:temp:places";
 
-    public static String listKey(Long tripId, int day) {
-        return String.format(LIST_KEY_FORMAT, tripId, day);
+    public static String dayPlacesKey(Long tripId, int day) {
+        return String.format(DAY_PLACES_KEY_FORMAT, tripId, day);
     }
 
-    public static String setKey(Long tripId, int day) {
-        return String.format(SET_KEY_FORMAT, tripId, day);
+    public static String dayPlaceSetKey(Long tripId, int day) {
+        return String.format(DAY_PLACE_SET_KEY_FORMAT, tripId, day);
     }
 
     public static String tempListKey(Long tripId) {
