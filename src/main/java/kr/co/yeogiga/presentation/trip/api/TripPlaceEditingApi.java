f453adc05ceb53f173f5ecab027740f2e863b2f7
DaeYoung0726 <pdy000726@naver.com>
2025-05-05 00:09:19 +0900

    docs: swagger 오류 문구 재작성

--- src/main/java/kr/co/yeogiga/presentation/trip/api/TripPlaceEditingApi.java
+++ src/main/java/kr/co/yeogiga/presentation/trip/api/TripPlaceEditingApi.java
@@ -98,8 +98,8 @@ public interface TripPlaceEditingApi {
                     content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject(value = """
                                         {
-                                            "code": "T004",
-                                            "message": "임시 저장소에 해당 장소가 존재하지 않습니다."
+                                            "code": "T002",
+                                            "message": "이미 추가된 장소입니다."
                                         }
                                     """)
                     })),
@@ -107,8 +107,8 @@ public interface TripPlaceEditingApi {
                     content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject(value = """
                                         {
-                                            "code": "T003",
-                                            "message": "이미 추가된 장소입니다."
+                                            "code": "T004",
+                                            "message": "임시 저장소에 해당 장소가 존재하지 않습니다."
                                         }
                                     """)
                     }))
