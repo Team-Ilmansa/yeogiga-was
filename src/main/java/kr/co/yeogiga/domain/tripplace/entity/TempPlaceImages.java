cb7d2334ac8aa64d5ffec7992047efb65a7c5d56
DaeYoung0726 <pdy000726@naver.com>
2025-05-05 17:11:52 +0900

    feat: 이미지 임시 저장소 생성

---
+++ src/main/java/kr/co/yeogiga/domain/tripplace/entity/TempPlaceImages.java
@@ -0,0 +1,31 @@
+package kr.co.yeogiga.domain.tripplace.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "temp_place_images")
+public class TempPlaceImages {
+    @Id
+    private String id;
+    private String placeId;
+    private List<Image> images;
+
+    @Builder
+    public TempPlaceImages(String placeId) {
+        this.placeId = placeId;
+        this.images = new ArrayList<>();
+    }
+
+    public void addImage(Image image) {
+        this.images.add(image);
+    }
+}
