174f80eed7b991cc3f107c79baee30a6548d9c3d
DaeYoung0726 <pdy000726@naver.com>
2025-05-09 01:12:57 +0900

    refact: 클래스명 변경

--- src/main/java/kr/co/yeogiga/application/tripplace/service/TripPlaceImageMovementService.java
+++ src/main/java/kr/co/yeogiga/application/tripplace/service/TripPlaceImageMovementService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TripPlaceImageCommandService {
+public class TripPlaceImageMovementService {
     private final TripDayPlaceService tripDayPlaceService;
 
     /**
