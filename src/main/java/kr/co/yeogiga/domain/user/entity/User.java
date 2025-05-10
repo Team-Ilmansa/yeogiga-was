d0a9d3735e060fcb1480b3e41702209d94d552cd
hky035 <rldud035@naver.com>
2025-05-10 20:21:30 +0900

    fix: users 테이블명 변경으로 인한 @SQLDelete 쿼리문 변경

--- src/main/java/kr/co/yeogiga/domain/user/entity/User.java
+++ src/main/java/kr/co/yeogiga/domain/user/entity/User.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @Entity(name = "users")
 @DynamicUpdate
 @SQLRestriction("deleted_at IS NULL")
-@SQLDelete(sql = "UPDATE user SET deleted_at = NOW() WHERE id = ?")
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE id = ?")
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
