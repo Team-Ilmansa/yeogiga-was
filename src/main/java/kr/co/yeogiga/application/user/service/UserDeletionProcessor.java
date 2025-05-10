96ac218383666d066fc2799ac2f55b61624b2447
hky035 <rldud035@naver.com>
2025-05-10 22:34:19 +0900

    docs: 주석 작성

--- src/main/java/kr/co/yeogiga/application/user/service/UserDeletionProcessor.java
+++ src/main/java/kr/co/yeogiga/application/user/service/UserDeletionProcessor.java
@@ -13,6 +13,11 @@ import java.util.List;
 public class UserDeletionProcessor {
     private final UserService userService;
 
+    /**
+     * 탈퇴 보류 기간 후 사용자 정보 완전 삭제 메서드
+     * - Soft Delete 정책에 따른 탈퇴 7일 이후 사용자 정보 삭제
+     * - 사용자 관련 정보 일괄 삭제 예졍
+     */
     @Transactional
     public void process() {
         LocalDate dateBeforeOneWeek = LocalDate.now().minusDays(7);
