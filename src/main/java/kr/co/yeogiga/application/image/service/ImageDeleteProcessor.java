54525222ad1e281efbb29b0f85d47c932fe6b313
DaeYoung0726 <pdy000726@naver.com>
2025-05-10 16:24:11 +0900

    feat: 임시 이미지 삭제 로직 구현

--- src/main/java/kr/co/yeogiga/application/image/service/ImageDeleteProcessor.java
+++ src/main/java/kr/co/yeogiga/application/image/service/ImageDeleteProcessor.java
@@ -1,15 +1,26 @@
 package kr.co.yeogiga.application.image.service;
 
-import kr.co.yeogiga.application.image.dto.ImageUrlDto;
+import kr.co.yeogiga.infrastructure.s3.AwsS3Storage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ImageDeleteProcessor {
-    private final ImageProcessingService imageProcessingService;
+    private final AwsS3Storage awsS3Storage;
 
-    public void process(ImageUrlDto imageUrlDto) {
-        imageProcessingService.processImageDeletion(imageUrlDto.urls());
+    /**
+     * 이미지 삭제를 비동기로 처리 메서드
+     *
+     * @param imageUrls : 이미지 url List
+     */
+    @Async
+    public void process(List<String> imageUrls) {
+        for (String url : imageUrls) {
+            awsS3Storage.deleteImage(url);
+        }
     }
 }
--- src/main/java/kr/co/yeogiga/application/image/service/TempPlaceImagesCommandService.java
+++ src/main/java/kr/co/yeogiga/application/image/service/TempPlaceImagesCommandService.java
@@ -1,5 +1,6 @@
 package kr.co.yeogiga.application.image.service;
 
+import kr.co.yeogiga.application.image.dto.ImageDeleteDto;
 import kr.co.yeogiga.domain.tripplace.entity.Image;
 import kr.co.yeogiga.domain.tripplace.service.TempPlaceImagesService;
 import lombok.RequiredArgsConstructor;
@@ -9,14 +10,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TempPlaceImagesCommandService {
     private final TempPlaceImagesService tempPlaceImagesService;
+    private final ImageDeleteProcessor imageDeleteProcessor;
 
     /**
-     * 지정된 placeId를 가진 TempPlaceImages 문서에 이미지를 추가하는 메서드
+     * 지정된 tripDayPlaceId 가진 TempPlaceImages 문서에 이미지를 추가하는 메서드
      *
      * @param tripDayPlaceId 추가할 이미지가 속할 여행일차 ID
      * @param image          MongoDB에 저장할 이미지 객체
      */
-    public void addImageToPlace(String tripDayPlaceId, Image image) {
+    public void addImageToTripDayPlace(String tripDayPlaceId, Image image) {
         tempPlaceImagesService.saveImage(tripDayPlaceId, image);
+    }
+
+    /**
+     * TempPlaceImages(임시 저장소) 문서에서 이미지를 제거하는 메서드
+     * - MongoDB 문서에서 이미지 삭제 - imageIds
+     * - AWS S3에서 이미지 삭제 - urls
+     *
+     * @param tempPlaceImageId 임시 저장소 ID
+     * @param deleteDto        이미지 삭제 정보 객체
+     */
+    public void removeTempImages(String tempPlaceImageId, ImageDeleteDto deleteDto) {
+        tempPlaceImagesService.deleteImages(tempPlaceImageId, deleteDto.imageIds());
+        imageDeleteProcessor.process(deleteDto.urls());
     }
 }
