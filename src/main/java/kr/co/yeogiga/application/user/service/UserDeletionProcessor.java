87b6bacdc92af7a6bc7a4ceb62bb623c52020d0f
hky035 <rldud035@naver.com>
2025-05-10 23:28:20 +0900

    docs: UserDeletionProcessor 내 주석 수정

--- src/main/java/kr/co/yeogiga/application/user/service/UserDeletionProcessor.java
+++ src/main/java/kr/co/yeogiga/application/user/service/UserDeletionProcessor.java
@@ -16,7 +16,7 @@ public class UserDeletionProcessor {
     /**
      * 탈퇴 보류 기간 후 사용자 정보 완전 삭제 메서드
      * - Soft Delete 정책에 따른 탈퇴 7일 이후 사용자 정보 삭제
-     * - 사용자 관련 정보 일괄 삭제 예졍
+     * - 사용자 관련 정보 일괄 삭제
      */
     @Transactional
     public void process() {
@@ -27,7 +27,7 @@ public class UserDeletionProcessor {
             return;
         }
 
-        /* TODO: 차후 연관 데이터 삭제 예정*/
+        /* TODO: 차후 연관 데이터 삭제 예정 */
         userService.deleteHardAllByIds(userIds);
     }
 }
