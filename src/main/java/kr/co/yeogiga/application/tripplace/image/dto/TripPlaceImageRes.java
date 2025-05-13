56599b460ed5ec84dbfb0ca3805a707332801753
DaeYoung0726 <pdy000726@naver.com>
2025-05-14 01:09:50 +0900

    feat: 상황별 이미지 반환 dto 추가

---
+++ src/main/java/kr/co/yeogiga/application/tripplace/image/dto/TripPlaceImageRes.java
@@ -0,0 +1,63 @@
+package kr.co.yeogiga.application.tripplace.image.dto;
+
+import kr.co.yeogiga.domain.tripplace.entity.Image;
+import kr.co.yeogiga.domain.tripplace.entity.Place;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TripPlaceImageRes {
+
+    public record PlaceImageInfo(
+            String id,
+            String name,
+            double latitude,
+            double longitude,
+            String placeType,
+            List<ImageDto> images
+    ) {
+        public static PlaceImageInfo from(Place place) {
+            return new PlaceImageInfo(
+                    place.getId(),
+                    place.getName(),
+                    place.getLatitude(),
+                    place.getLongitude(),
+                    place.getPlaceType(),
+                    place.getImages().stream()
+                            .map(ImageDto::from)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
+    public record UnmatchedImageInfo(
+            List<ImageDto> images
+    ) {
+        public static UnmatchedImageInfo from(List<Image> unmatchedImages) {
+            return new UnmatchedImageInfo(
+                    unmatchedImages.stream()
+                            .map(ImageDto::from)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
+    public record ImageDto(
+            String id,
+            String url,
+            Double latitude,
+            Double longitude,
+            LocalDateTime date
+    ) {
+        public static ImageDto from(Image image) {
+            return new ImageDto(
+                    image.getId(),
+                    image.getUrl(),
+                    image.getLatitude(),
+                    image.getLongitude(),
+                    image.getDate()
+            );
+        }
+    }
+}
