fdb404662c8e4fe114ac686f6b0e977d0cdbdb8b
hky035 <hky035@gmail.com>
2025-05-16 02:06:35 +0900

    docs: 여행 시간 수정 api Swagger 문서 작성

--- src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
+++ src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -30,17 +31,21 @@ public class TripReq {
     }
 
     @Builder
+    @Schema(name = "TripReq.Time", description = "여행 시간 수정 요청 DTO")
     public record Time(
+            @Schema(description = "여행 시작 시각", example = "2025-01-01T12:00:00", type = "string")
             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
             @JsonDeserialize(using = LocalDateTimeDeserializer.class)
             @JsonSerialize(using = LocalDateTimeSerializer.class)
             LocalDateTime start,
 
+            @Schema(description = "여행 종료 시각", example = "2025-01-05T12:00:00", type = "string")
             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
             @JsonDeserialize(using = LocalDateTimeDeserializer.class)
             @JsonSerialize(using = LocalDateTimeSerializer.class)
             LocalDateTime end
     ) {
+        @Schema(hidden = true)
         public boolean isValid() {
             return start.isBefore(end);
         }
