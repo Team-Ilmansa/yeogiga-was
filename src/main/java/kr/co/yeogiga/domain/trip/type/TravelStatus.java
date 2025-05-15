82977ee13682b603ab7ae834c6ed240040daa8b9
hky035 <hky035@gmail.com>
2025-05-16 01:38:46 +0900

    feat: TravelStatus 내 상태값 생성 메서드 추가

--- src/main/java/kr/co/yeogiga/domain/trip/type/TravelStatus.java
+++ src/main/java/kr/co/yeogiga/domain/trip/type/TravelStatus.java
@@ -1,7 +1,27 @@
 package kr.co.yeogiga.domain.trip.type;
 
+import java.time.LocalDateTime;
+
 public enum TravelStatus {
     PLANNED,
     IN_PROGRESS,
-    COMPLETED
+    COMPLETED;
+
+    public static TravelStatus resolveStatus(LocalDateTime start, LocalDateTime end) {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.isBefore(start)) {
+            return PLANNED;
+        }
+
+        if (start.isBefore(now) && end.isAfter(now)) {
+            return IN_PROGRESS;
+        }
+
+        if (now.isAfter(end)) {
+            return COMPLETED;
+        }
+
+        return PLANNED;
+    }
 }
