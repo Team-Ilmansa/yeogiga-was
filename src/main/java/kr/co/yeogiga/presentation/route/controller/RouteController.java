6cf46ced8bc28828d201b298a25a0a827bbf9130
DaeYoung0726 <pdy000726@naver.com>
2025-05-16 00:03:10 +0900

    chore: Override 애너테이션 추가

--- src/main/java/kr/co/yeogiga/presentation/route/controller/RouteController.java
+++ src/main/java/kr/co/yeogiga/presentation/route/controller/RouteController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RouteController implements RouteApi {
     private final TripLeaderCommandService tripLeaderCommandService;
 
+    @Override
     @PostMapping("/{tripId}/days/{day}/routes")
     public ResponseEntity<?> storeLeaderRoute(
             @PathVariable Long tripId,
