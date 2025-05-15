1fc9c39a510652664e50d328ef7f9ebb96310461
hky035 <hky035@gmail.com>
2025-05-16 00:16:38 +0900

    test: 여행 생성 시 방장 여행 멤버 추가로 인한 Mock 객체 추가

--- src/test/java/kr/co/yeogiga/application/trip/service/TripCommandServiceTest.java
+++ src/test/java/kr/co/yeogiga/application/trip/service/TripCommandServiceTest.java
@@ -2,8 +2,13 @@ package kr.co.yeogiga.application.trip.service;
 
 import kr.co.yeogiga.application.trip.dto.TripReq;
 import kr.co.yeogiga.domain.trip.entity.Trip;
+import kr.co.yeogiga.domain.trip.entity.TripMember;
+import kr.co.yeogiga.domain.trip.service.TripMemberService;
 import kr.co.yeogiga.domain.trip.service.TripService;
 import kr.co.yeogiga.domain.trip.type.TravelStatus;
+import kr.co.yeogiga.domain.user.entity.User;
+import kr.co.yeogiga.domain.user.service.UserService;
+import kr.co.yeogiga.domain.user.type.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,15 +19,25 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TripCommandServiceTest {
     @Mock
     private TripService tripService;
+
+    @Mock
+    private TripMemberService tripMemberService;
+
+    @Mock
+    private UserService userService;
 
     @InjectMocks
     private TripCommandService tripCommandService;
@@ -44,7 +59,18 @@ public class TripCommandServiceTest {
         @DisplayName("성공")
         void success() {
             // given
+            User user = User.builder()
+                    .username("username")
+                    .password("password")
+                    .nickname("nickname")
+                    .email("email")
+                    .role(Role.USER)
+                    .build();
+
+            when(userService.readById(any())).thenReturn(Optional.of(user));
             doNothing().when(tripService).save(any());
+            doNothing().when(tripMemberService).save(any());
+
 
             // when
             tripCommandService.create(leaderId, creationRequest);
