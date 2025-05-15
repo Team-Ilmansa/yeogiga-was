9ca0af9c97174dd5fa04126b16bab2055ce2e465
hky035 <hky035@gmail.com>
2025-05-15 22:32:48 +0900

    test: 여행 생성 테스트 코드 작성

---
+++ src/test/java/kr/co/yeogiga/application/trip/service/TripCommandServiceTest.java
@@ -0,0 +1,62 @@
+package kr.co.yeogiga.application.trip.service;
+
+import kr.co.yeogiga.application.trip.dto.TripReq;
+import kr.co.yeogiga.domain.trip.entity.Trip;
+import kr.co.yeogiga.domain.trip.service.TripService;
+import kr.co.yeogiga.domain.trip.type.TravelStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TripCommandServiceTest {
+    @Mock
+    private TripService tripService;
+
+    @InjectMocks
+    private TripCommandService tripCommandService;
+
+    @Nested
+    @DisplayName("여행 생성")
+    class TripCreation {
+        private final Long leaderId = 1L;
+
+        private TripReq.Creation creationRequest = TripReq.Creation.builder()
+                .title("test")
+                .city("대구광역시")
+                .build();
+
+        @Captor
+        private ArgumentCaptor<Trip> tripCaptor;
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            doNothing().when(tripService).save(any());
+
+            // when
+            tripCommandService.create(leaderId, creationRequest);
+
+            // then
+            verify(tripService).save(tripCaptor.capture());
+
+            Trip capturedTrip = tripCaptor.getValue();
+            assertEquals(creationRequest.title(), capturedTrip.getTitle());
+            assertEquals(creationRequest.city(), capturedTrip.getCity());
+            assertEquals(leaderId, capturedTrip.getLeaderId());
+            assertEquals(TravelStatus.PLANNED, capturedTrip.getTravelStatus());
+        }
+    }
+}
