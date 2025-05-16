24b15474ba6ba274ad30cb08cffdfe614621f2ae
hky035 <hky035@gmail.com>
2025-05-16 11:31:26 +0900

    chore: 시간 수정 요청 내 어노테이션 제거에 따른 불필요 import문 수정

--- src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
+++ src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
@@ -1,10 +1,5 @@
 package kr.co.yeogiga.application.trip.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
