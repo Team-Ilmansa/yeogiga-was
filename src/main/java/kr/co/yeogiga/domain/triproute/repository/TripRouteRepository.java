5a0d2202ffc42a7db03b64208c747e0f482c3f28
DaeYoung0726 <pdy000726@naver.com>
2025-05-17 01:40:53 +0900

    feat: 이동 경로 조회 로직 구현

--- src/main/java/kr/co/yeogiga/domain/triproute/repository/TripRouteRepository.java
+++ src/main/java/kr/co/yeogiga/domain/triproute/repository/TripRouteRepository.java
@@ -3,5 +3,8 @@ package kr.co.yeogiga.domain.triproute.repository;
 import kr.co.yeogiga.domain.triproute.entity.TripRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TripRouteRepository extends JpaRepository<TripRoute, Long>, CustomTripRouteRepository {
+    Optional<TripRoute> findByTripIdAndDay(Long tripId, int day);
 }
