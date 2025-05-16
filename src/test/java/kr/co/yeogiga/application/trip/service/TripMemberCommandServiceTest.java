fe722db4cef67c54749e4fb7b68942c4d237725a
hky035 <rldud035@naver.com>
2025-05-17 02:23:17 +0900

    test: 여행 참가 로직 테스트 코드 작성

---
+++ src/test/java/kr/co/yeogiga/application/trip/service/TripMemberCommandServiceTest.java
@@ -0,0 +1,129 @@
+package kr.co.yeogiga.application.trip.service;
+
+import kr.co.yeogiga.common.exception.CustomException;
+import kr.co.yeogiga.domain.trip.entity.Trip;
+import kr.co.yeogiga.domain.trip.entity.TripMember;
+import kr.co.yeogiga.domain.trip.exception.TripErrorType;
+import kr.co.yeogiga.domain.trip.exception.TripMemberErrorType;
+import kr.co.yeogiga.domain.trip.service.TripMemberService;
+import kr.co.yeogiga.domain.trip.service.TripService;
+import kr.co.yeogiga.domain.trip.type.TravelStatus;
+import kr.co.yeogiga.domain.user.entity.User;
+import kr.co.yeogiga.domain.user.exception.UserErrorType;
+import kr.co.yeogiga.domain.user.service.UserService;
+import kr.co.yeogiga.domain.user.type.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TripMemberCommandServiceTest {
+
+    @Mock
+    private TripMemberService tripMemberService;
+
+    @Mock
+    private TripService tripService;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private TripMemberCommandService tripMemberCommandService;
+
+    @Nested
+    @DisplayName("여행 멤버 참가")
+    class JoinTrip {
+        private final Long userId = 1L;
+        private final Long tripId = 1L;
+
+        private final Trip trip = Trip.builder()
+                .title("title")
+                .city("대구광역시")
+                .leaderId(userId)
+                .travelStatus(TravelStatus.IN_PROGRESS)
+                .build();
+
+        private final User user = User.builder()
+                .username("username")
+                .password("password")
+                .email("test@test.com")
+                .nickname("nickname")
+                .role(Role.USER)
+                .build();
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            when(tripService.readById(tripId)).thenReturn(Optional.of(trip));
+            when(userService.readById(userId)).thenReturn(Optional.of(user));
+            when(tripMemberService.existsByTripIdAndUserId(tripId, userId)).thenReturn(false);
+
+            // when
+            tripMemberCommandService.joinTrip(tripId, userId);
+
+            // then
+            verify(tripMemberService, times(1)).save(isA(TripMember.class));
+        }
+
+        @Test
+        @DisplayName("실패 - 존재하지 않는 여행")
+        void failNotFoundTrip() {
+            // given
+            when(tripService.readById(tripId)).thenReturn(Optional.empty());
+
+            // when
+            CustomException exception = assertThrows(CustomException.class, ()
+                    -> tripMemberCommandService.joinTrip(tripId, userId));
+
+            // then
+            assertEquals(TripErrorType.TRIP_NOT_FOUND, exception.getErrorType());
+        }
+
+        @Test
+        @DisplayName("실패 - 존재하지 않는 사용자")
+        void failNotFoundUser() {
+            // given
+            when(tripService.readById(tripId)).thenReturn(Optional.of(trip));
+            when(userService.readById(userId)).thenReturn(Optional.empty());
+
+            // when
+            CustomException exception = assertThrows(CustomException.class, ()
+                    -> tripMemberCommandService.joinTrip(tripId, userId));
+
+            // then
+            assertEquals(UserErrorType.NOT_FOUND, exception.getErrorType());
+        }
+
+        @Test
+        @DisplayName("실패 - 이미 여행에 참가 중인 사용자")
+        void failAlreadyExists() {
+            // given
+            when(tripService.readById(tripId)).thenReturn(Optional.of(trip));
+            when(userService.readById(userId)).thenReturn(Optional.of(user));
+            when(tripMemberService.existsByTripIdAndUserId(tripId, userId)).thenReturn(true);
+
+            // when
+            CustomException exception = assertThrows(CustomException.class, ()
+                    -> tripMemberCommandService.joinTrip(tripId, userId));
+
+            // then
+            assertEquals(TripMemberErrorType.ALREADY_EXISTS, exception.getErrorType());
+        }
+    }
+}
