651d50fc46f08f7cbf0dec7ce618216f7bcf1e35
hky035 <rldud035@naver.com>
2025-05-17 02:25:52 +0900

    docs: 여행 참가 메서드 주석 작성

--- src/main/java/kr/co/yeogiga/application/trip/service/TripMemberCommandService.java
+++ src/main/java/kr/co/yeogiga/application/trip/service/TripMemberCommandService.java
@@ -20,6 +20,16 @@ public class TripMemberCommandService {
     private final TripService tripService;
     private final UserService userService;
 
+    /**
+     * 여행 멤버에 사용자를 등록(참가)하는 메서드
+     *
+     * @param tripId    여행 ID
+     * @param userId    사용자 ID
+     *
+     * @throws CustomException  TripErrorType.TRIP_NOT_FOUND - 존재하지 않는 여행
+     * @throws CustomException  UserErrorType.NOT_FOUND - 존재하지 않는 사용자
+     * @throws CustomException  TripMemberErrorType.ALREADY_EXISTS - 이미 여행에 참가 중인 사용자
+     */
     public void joinTrip(Long tripId, Long userId) {
         Trip trip = tripService.readById(tripId)
                 .orElseThrow(() -> new CustomException(TripErrorType.TRIP_NOT_FOUND));
