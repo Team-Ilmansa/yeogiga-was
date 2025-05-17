3583f91fd5c8e71aeac23cfd095f47a2f03e91a5
DaeYoung0726 <pdy000726@naver.com>
2025-05-17 18:03:41 +0900

    feat: 여행 루트 조회 로직 구현

--- src/main/java/kr/co/yeogiga/domain/triproute/repository/TripRouteRepository.java
+++ src/main/java/kr/co/yeogiga/domain/triproute/repository/TripRouteRepository.java
@@ -3,8 +3,10 @@ package kr.co.yeogiga.domain.triproute.repository;
 import kr.co.yeogiga.domain.triproute.entity.TripRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TripRouteRepository extends JpaRepository<TripRoute, Long>, CustomTripRouteRepository {
     Optional<TripRoute> findByTripIdAndDay(Long tripId, int day);
+    List<TripRoute> findByTripIdOrderByDay(Long tripId);
 }
