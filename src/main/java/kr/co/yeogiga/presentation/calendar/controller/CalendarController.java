2d6529a9bd5ea07e58daddc585daae096b988489
DaeYoung0726 <pdy000726@naver.com>
2025-05-18 22:13:56 +0900

    feat: W2M 읽기 연산 rest api 연결

--- src/main/java/kr/co/yeogiga/presentation/calendar/controller/CalendarController.java
+++ src/main/java/kr/co/yeogiga/presentation/calendar/controller/CalendarController.java
@@ -3,6 +3,7 @@ package kr.co.yeogiga.presentation.calendar.controller;
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.calendar.dto.CalendarReq;
 import kr.co.yeogiga.application.calendar.service.CalendarCommandService;
+import kr.co.yeogiga.application.calendar.service.CalendarQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.common.security.auth.CustomUserDetails;
 import kr.co.yeogiga.presentation.calendar.api.CalendarApi;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CalendarController implements CalendarApi {
     private final CalendarCommandService calendarCommandService;
+    private final CalendarQueryService calendarQueryService;
 
     @Override
     @PostMapping("/{tripId}/calendars")
@@ -32,6 +35,29 @@ public class CalendarController implements CalendarApi {
     ) {
         calendarCommandService.create(userDetails.getUserId(), tripId, calendarReq);
         return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.created());
+    }
+
+    @GetMapping("/{tripId}/calendars/me")
+    public ResponseEntity<?> getUserAvailability(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long tripId
+    ) {
+        return ResponseEntity.ok(
+                SuccessResponse.from(
+                        calendarQueryService.getUserAvailability(userDetails.getUserId(), tripId)
+                )
+        );
+    }
+
+    @GetMapping("/{tripId}/calendars")
+    public ResponseEntity<?> getTripAvailabilities(
+            @PathVariable Long tripId
+    ) {
+        return ResponseEntity.ok(
+                SuccessResponse.from(
+                        calendarQueryService.getTripAvailabilities(tripId)
+                )
+        );
     }
 
     @Override
