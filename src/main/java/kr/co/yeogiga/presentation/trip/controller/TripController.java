16026c5e995f4bde6ecb12ae53c7926ffcd60817
hky035 <hky035@gmail.com>
2025-05-19 22:47:42 +0900

    feat: 사용자가 속한 모든 여행 조회 rest api 구현

--- src/main/java/kr/co/yeogiga/presentation/trip/controller/TripController.java
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripController.java
@@ -2,7 +2,9 @@ package kr.co.yeogiga.presentation.trip.controller;
 
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.trip.dto.TripReq;
+import kr.co.yeogiga.application.trip.dto.TripRes;
 import kr.co.yeogiga.application.trip.service.TripCommandService;
+import kr.co.yeogiga.application.trip.service.TripQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.common.security.auth.CustomUserDetails;
 import kr.co.yeogiga.presentation.trip.api.TripApi;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,11 +20,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/trip")
 @RequiredArgsConstructor
 public class TripController implements TripApi {
+    private final TripQueryService tripQueryService;
     private final TripCommandService tripCommandService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllTrip(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<TripRes.TripSummary> tripList = tripQueryService.getAllTrip(userDetails.getUserId());
+        return ResponseEntity.ok().body(SuccessResponse.from(tripList));
+    }
 
     @Override
     @PostMapping
