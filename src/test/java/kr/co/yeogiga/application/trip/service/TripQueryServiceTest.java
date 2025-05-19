6cff7fd2882768ca6e61ac1dcac3d488dff01242
hky035 <hky035@gmail.com>
2025-05-19 22:47:27 +0900

    test: 사용자가 속한 모든 여행 조회 로직 테스트 코드 작성

---
+++ src/test/java/kr/co/yeogiga/application/trip/service/TripQueryServiceTest.java
@@ -0,0 +1,86 @@
+package kr.co.yeogiga.application.trip.service;
+
+import kr.co.yeogiga.application.trip.dto.TripRes;
+import kr.co.yeogiga.domain.trip.entity.Trip;
+import kr.co.yeogiga.domain.trip.entity.TripMember;
+import kr.co.yeogiga.domain.trip.service.TripMemberService;
+import kr.co.yeogiga.domain.trip.type.TravelStatus;
+import kr.co.yeogiga.domain.user.entity.User;
+import kr.co.yeogiga.domain.user.type.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TripQueryServiceTest {
+
+    @Mock
+    private TripMemberService tripMemberService;
+
+    @InjectMocks
+    private TripQueryService tripQueryService;
+
+    @Nested
+    @DisplayName("전체 여행 목록 조회")
+    class GetAllTrip {
+        private final Long userId = 1L;
+
+        private Trip trip = Trip.builder()
+                .title("title")
+                .city("대구광역시")
+                .leaderId(userId)
+                .travelStatus(TravelStatus.PLANNED)
+                .build();
+
+        private User user = User.builder()
+                .username("username")
+                .password("password")
+                .nickname("nickname")
+                .email("test@test.com")
+                .role(Role.USER)
+                .build();
+
+        private TripMember tripMember = TripMember.builder()
+                .trip(trip)
+                .user(user)
+                .build();
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            when(tripMemberService.readAllByUserId(userId)).thenReturn(List.of(tripMember));
+
+            // when
+            List<TripRes.TripSummary> result = tripQueryService.getAllTrip(userId);
+
+            // then
+            assertThat(result).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("성공 - 빈 배열 반환")
+        void successEmptyList() {
+            // given
+            when(tripMemberService.readAllByUserId(userId)).thenReturn(List.of());
+
+            // when
+            List<TripRes.TripSummary> result = tripQueryService.getAllTrip(userId);
+
+            // then
+            assertThat(result).hasSize(0);
+        }
+    }
+
+
+}
