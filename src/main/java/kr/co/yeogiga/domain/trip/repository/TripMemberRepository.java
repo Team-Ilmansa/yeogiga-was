e598818ae658f29211b6c1b372b192d6d5b651f5
hky035 <hky035@gmail.com>
2025-05-21 02:09:40 +0900

    feat: 사용자 id를 통한 여행 조회 쿼리 메서드 추가

--- src/main/java/kr/co/yeogiga/domain/trip/repository/TripMemberRepository.java
+++ src/main/java/kr/co/yeogiga/domain/trip/repository/TripMemberRepository.java
@@ -1,8 +1,16 @@
 package kr.co.yeogiga.domain.trip.repository;
 
+import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.domain.trip.entity.TripMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
     boolean existsByTripIdAndUserId(Long tripId, Long userId);
+
+    @Query("SELECT tm.trip FROM trip_member tm WHERE tm.user.id = :userId ORDER BY tm.trip.startedAt ASC NULLS LAST")
+    List<Trip> findAllTripByUserId(@Param(value = "userId") Long userId);
 }
--- src/main/java/kr/co/yeogiga/domain/trip/service/TripMemberService.java
+++ src/main/java/kr/co/yeogiga/domain/trip/service/TripMemberService.java
@@ -1,9 +1,12 @@
 package kr.co.yeogiga.domain.trip.service;
 
+import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.domain.trip.entity.TripMember;
 import kr.co.yeogiga.domain.trip.repository.TripMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +15,10 @@ public class TripMemberService {
 
     public void save(TripMember tripMember) {
         tripMemberRepository.save(tripMember);
+    }
+
+    public List<Trip> readAllTripByUserId(Long userId) {
+        return tripMemberRepository.findAllTripByUserId(userId);
     }
 
     public boolean existsByTripIdAndUserId(Long tripId, Long userId) {
