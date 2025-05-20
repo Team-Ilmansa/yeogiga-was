51eebec619c290e552e1387e70ecd0a3067628fd
hky035 <hky035@gmail.com>
2025-05-21 02:15:34 +0900

    feat: 메인 화면 내 여행 조회 rest api 구현

--- src/main/java/kr/co/yeogiga/presentation/trip/controller/TripController.java
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripController.java
@@ -2,14 +2,18 @@ package kr.co.yeogiga.presentation.trip.controller;
 
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.trip.dto.TripReq;
+import kr.co.yeogiga.application.trip.dto.TripRes;
 import kr.co.yeogiga.application.trip.service.TripCommandService;
+import kr.co.yeogiga.application.trip.service.TripQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.common.security.auth.CustomUserDetails;
+import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.presentation.trip.api.TripApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TripController implements TripApi {
     private final TripCommandService tripCommandService;
+    private final TripQueryService tripQueryService;
+
+    @GetMapping("/main")
+    public ResponseEntity<?> getMainTrip(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        TripRes.TripMainInfo tripMainInfo = tripQueryService.getTripMainInfo(userDetails.getUserId());
+        return ResponseEntity.ok().body(SuccessResponse.from(tripMainInfo));
+    }
 
     @Override
     @PostMapping
