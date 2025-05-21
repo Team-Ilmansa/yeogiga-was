2e3cff18b5cee9685f418d512c5588a098964ffd
DaeYoung0726 <pdy000726@naver.com>
2025-05-21 22:49:34 +0900

    refact: 불필요 상수 삭제

--- src/main/java/kr/co/yeogiga/infrastructure/redis/constant/PlaceConstant.java
+++ src/main/java/kr/co/yeogiga/infrastructure/redis/constant/PlaceConstant.java
@@ -6,7 +6,6 @@ public final class PlaceConstant {
 
     private static final String DAY_PLACES_KEY_FORMAT = "trip:%d:day:%d:places";
     private static final String DAY_PLACE_SET_KEY_FORMAT = "trip:%d:day:%d:places:set";
-    private static final String TEMP_LIST_KEY_FORMAT = "trip:%d:temp:places";
 
     public static String dayPlacesKey(Long tripId, int day) {
         return String.format(DAY_PLACES_KEY_FORMAT, tripId, day);
@@ -14,9 +13,5 @@ public final class PlaceConstant {
 
     public static String dayPlaceSetKey(Long tripId, int day) {
         return String.format(DAY_PLACE_SET_KEY_FORMAT, tripId, day);
-    }
-
-    public static String tempListKey(Long tripId) {
-        return String.format(TEMP_LIST_KEY_FORMAT, tripId);
     }
 }
