45677d3a5d5d6830278ed043cd5fc223e15bf297
hky035 <hky035@gmail.com>
2025-05-22 02:12:22 +0900

    feat: 여행 멤버 조회 시 여행 존재 여부 검증 로직 제거

--- src/main/java/kr/co/yeogiga/application/trip/service/TripMemberQueryService.java
+++ src/main/java/kr/co/yeogiga/application/trip/service/TripMemberQueryService.java
@@ -1,10 +1,7 @@
 package kr.co.yeogiga.application.trip.service;
 
 import kr.co.yeogiga.application.trip.dto.TripMemberRes;
-import kr.co.yeogiga.common.exception.CustomException;
-import kr.co.yeogiga.domain.trip.exception.TripErrorType;
 import kr.co.yeogiga.domain.trip.service.TripMemberService;
-import kr.co.yeogiga.domain.trip.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +12,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TripMemberQueryService {
     private final TripMemberService tripMemberService;
-    private final TripService tripService;
 
     /**
      * 여행 ID를 통해 모든 여행 멤버를 조회하는 메서드
@@ -25,10 +21,6 @@ public class TripMemberQueryService {
      */
     @Transactional(readOnly = true)
     public List<TripMemberRes.MemberInfo> getTripMembers(Long tripId) {
-        if (!tripService.existsById(tripId)) {
-            throw new CustomException(TripErrorType.TRIP_NOT_FOUND);
-        }
-
         return tripMemberService.readAllUserByTripId(tripId).stream()
                 .map(TripMemberRes.MemberInfo::fromEntity)
                 .toList();
