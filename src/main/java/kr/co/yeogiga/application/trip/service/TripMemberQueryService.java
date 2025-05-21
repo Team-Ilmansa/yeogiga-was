779a44f373c3beff148ec08d310467ebb522f596
hky035 <hky035@gmail.com>
2025-05-22 00:03:36 +0900

    feat: 여행 멤버 조회 시 여행 존재 여부 확인 로직 추가

--- src/main/java/kr/co/yeogiga/application/trip/service/TripMemberQueryService.java
+++ src/main/java/kr/co/yeogiga/application/trip/service/TripMemberQueryService.java
@@ -1,9 +1,13 @@
 package kr.co.yeogiga.application.trip.service;
 
 import kr.co.yeogiga.application.trip.dto.TripMemberRes;
+import kr.co.yeogiga.common.exception.CustomException;
+import kr.co.yeogiga.domain.trip.exception.TripErrorType;
 import kr.co.yeogiga.domain.trip.service.TripMemberService;
+import kr.co.yeogiga.domain.trip.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TripMemberQueryService {
     private final TripMemberService tripMemberService;
+    private final TripService tripService;
 
     /**
      * 여행 ID를 통해 모든 여행 멤버를 조회하는 메서드
@@ -18,7 +23,12 @@ public class TripMemberQueryService {
      * @param tripId        여행 ID
      * @return              여행 멤버 목록
      */
+    @Transactional(readOnly = true)
     public List<TripMemberRes.MemberInfo> getTripMembers(Long tripId) {
+        if (!tripService.existsById(tripId)) {
+            throw new CustomException(TripErrorType.TRIP_NOT_FOUND);
+        }
+
         return tripMemberService.readAllUserByTripId(tripId).stream()
                 .map(TripMemberRes.MemberInfo::fromEntity)
                 .toList();
