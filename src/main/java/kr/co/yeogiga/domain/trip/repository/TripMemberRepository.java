a0f5c8f9223f333ada6f29dc132872ca773102a4
hky035 <hky035@gmail.com>
2025-05-21 16:36:04 +0900

    feat: 사용자 ID를 통한 모든 여행 조회 쿼리 메서드 내 누락 어노테이션 추가

--- src/main/java/kr/co/yeogiga/domain/trip/repository/TripMemberRepository.java
+++ src/main/java/kr/co/yeogiga/domain/trip/repository/TripMemberRepository.java
@@ -4,6 +4,7 @@ import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.domain.trip.entity.TripMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +12,5 @@ public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
     boolean existsByTripIdAndUserId(Long tripId, Long userId);
 
     @Query("SELECT tm.trip FROM trip_member tm WHERE tm.user.id = :userId ORDER BY tm.trip.startedAt ASC NULLS LAST")
-    List<Trip> findAllTripByUserId(Long userId);
+    List<Trip> findAllTripByUserId(@Param(value = "userId") Long userId);
 }
