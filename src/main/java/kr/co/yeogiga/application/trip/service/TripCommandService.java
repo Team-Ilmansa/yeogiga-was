8df3a7dbebcd9a31d507b326b97ce534d79545f8
hky035 <hky035@gmail.com>
2025-05-24 03:20:09 +0900

    feat: SETTING 상태의 여행 시간 수정 시 상태 변경 미반영 로직 추가

--- src/main/java/kr/co/yeogiga/application/trip/service/TripCommandService.java
+++ src/main/java/kr/co/yeogiga/application/trip/service/TripCommandService.java
@@ -53,6 +53,7 @@ public class TripCommandService {
 
     /**
      * 여행 시간 수정 메서드
+     * - 여행 일정과 목적지까지 확정이 나지 않은 상태(SETTING)일 경우, 여행 상태(TravelStatus)값 미변경
      *
      * @param tripId        여행 ID
      * @param userId        요청자 ID
@@ -75,10 +76,12 @@ public class TripCommandService {
             throw new CustomException(TripErrorType.PERMISSION_DENIED_NOT_LEADER);
         }
 
-        TravelStatus status = TravelStatus.resolveStatus(time.start(), time.end());
+        if (trip.getTravelStatus() != TravelStatus.SETTING) {
+            TravelStatus status = TravelStatus.resolveStatus(time.start(), time.end());
+            trip.updateStatus(status);
+        }
 
         trip.updateTime(time.start(), time.end());
-        trip.updateStatus(status);
     }
 
     /**
