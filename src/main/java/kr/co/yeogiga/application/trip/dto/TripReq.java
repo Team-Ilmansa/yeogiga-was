6bd8958b863a9d13997ecddc400fccbdb190d302
hky035 <hky035@gmail.com>
2025-05-23 20:42:06 +0900

    feat: 여행 생성 요청 dto 내 city 값 제거

--- src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
+++ src/main/java/kr/co/yeogiga/application/trip/dto/TripReq.java
@@ -17,11 +17,7 @@ public class TripReq {
             @Schema(description = "여행 제목", example = "여기가 여행")
             @NotBlank(message = "제목은 필수 입력값입니다.")
             @Size(max = 20, message = "제목은 최대 20글자까지 가능합니다.")
-            String title,
-
-            @Schema(description = "목적지 도시", example = "대구광역시")
-            @Size(max = 20, message = "여행 도시는 최대 20글자까지 가능합니다.")
-            String city
+            String title
     ) {
     }
 
