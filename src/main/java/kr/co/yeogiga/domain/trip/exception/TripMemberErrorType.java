c49bdc84e90a80648d72888177b94c220a66c537
hky035 <hky035@gmail.com>
2025-05-25 00:34:58 +0900

    feat: 여행 멤버 관련 에러 타입 추가
     * 여행 방장이 권한 위임 없이 여행을 탈퇴하려는 경우
     * 여행 멤버가 아닌 경우

--- src/main/java/kr/co/yeogiga/domain/trip/exception/TripMemberErrorType.java
+++ src/main/java/kr/co/yeogiga/domain/trip/exception/TripMemberErrorType.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TripMemberErrorType implements BaseErrorType {
 
-    ALREADY_EXISTS(HttpStatus.CONFLICT, "T100", "이미 여행에 참가 중인 사용자입니다.");
+    ALREADY_EXISTS(HttpStatus.CONFLICT, "T100", "이미 여행에 참가 중인 사용자입니다."),
+    LEADER_CAN_NOT_LEAVE_TRIP(HttpStatus.BAD_REQUEST, "T101", "방장은 여행에서 떠날 수 없습니다. 권한을 위임해주세요."),
+    IS_NOT_MEMBER(HttpStatus.BAD_REQUEST, "T102", "해당 여행의 멤버가 아닙니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
