86c1042be6419a5e3e6a007f29d3de4714bbf389
hky035 <hky035@gmail.com>
2025-05-25 00:43:12 +0900

    feat: 여행 멤버 탈퇴 rest api 구현

--- src/main/java/kr/co/yeogiga/presentation/trip/controller/TripMemberController.java
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripMemberController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,5 +37,14 @@ public class TripMemberController implements TripMemberApi {
     ) {
         tripMemberCommandService.joinTrip(tripId, userDetails.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.created());
+    }
+
+    @DeleteMapping("/{tripId}/members")
+    public ResponseEntity<?> leaveTrip(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long tripId
+    ) {
+        tripMemberCommandService.leaveTrip(tripId, userDetails.getUserId());
+        return ResponseEntity.ok(SuccessResponse.ok());
     }
 }
