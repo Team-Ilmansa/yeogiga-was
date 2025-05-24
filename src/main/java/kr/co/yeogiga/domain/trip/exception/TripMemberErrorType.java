e737ecff019d3d14454a3c2074462b67fabe81f8
hky035 <hky035@gmail.com>
2025-05-25 02:53:15 +0900

    feat: 여행 멤버 관련 에러 타입 추가
     * 자기 자신 추방
     * 여행 방장만 이용 가능한 기능

--- src/main/java/kr/co/yeogiga/domain/trip/exception/TripMemberErrorType.java
+++ src/main/java/kr/co/yeogiga/domain/trip/exception/TripMemberErrorType.java
@@ -12,7 +12,9 @@ public enum TripMemberErrorType implements BaseErrorType {
 
     ALREADY_EXISTS(HttpStatus.CONFLICT, "T100", "이미 여행에 참가 중인 사용자입니다."),
     LEADER_CAN_NOT_LEAVE_TRIP(HttpStatus.BAD_REQUEST, "T101", "방장은 여행에서 떠날 수 없습니다. 권한을 위임해주세요."),
-    IS_NOT_MEMBER(HttpStatus.BAD_REQUEST, "T102", "해당 여행의 멤버가 아닙니다.");
+    IS_NOT_MEMBER(HttpStatus.BAD_REQUEST, "T102", "해당 여행의 멤버가 아닙니다."),
+    CAN_NOT_SELF_KICK(HttpStatus.BAD_REQUEST, "T103", "자기 자신은 추방할 수 없습니다."),
+    ONLY_LEADER(HttpStatus.FORBIDDEN, "T104", "여행 방장만 이용 가능한 기능입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
