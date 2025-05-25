05f4b43dfe2c3d740e04b5079c6726b352a1d13a
hky035 <rldud035@naver.com>
2025-05-26 00:32:13 +0900

    feat: Pin 생성 rest api 구현

---
+++ src/main/java/kr/co/yeogiga/presentation/pin/controller/PinController.java
@@ -0,0 +1,30 @@
+package kr.co.yeogiga.presentation.pin.controller;
+
+import kr.co.yeogiga.application.pin.dto.PinReq;
+import kr.co.yeogiga.application.pin.service.PinCommandService;
+import kr.co.yeogiga.common.response.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/trip")
+@RequiredArgsConstructor
+public class PinController {
+    private final PinCommandService pinCommandService;
+
+    @PostMapping("/{tripId}/pin")
+    public ResponseEntity<?> createPin(
+            @PathVariable Long tripId,
+            @RequestBody PinReq.Creation request
+    ) {
+        pinCommandService.createPin(tripId, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(SuccessResponse.created());
+    }
+}
