5f16ca135bb4c4d169ffd89799d61e240c68a873
hky035 <rldud035@naver.com>
2025-05-26 12:20:43 +0900

    feat: Pin 조회 rest api 구현

--- src/main/java/kr/co/yeogiga/presentation/pin/controller/PinController.java
+++ src/main/java/kr/co/yeogiga/presentation/pin/controller/PinController.java
@@ -3,11 +3,13 @@ package kr.co.yeogiga.presentation.pin.controller;
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.pin.dto.PinReq;
 import kr.co.yeogiga.application.pin.service.PinCommandService;
+import kr.co.yeogiga.application.pin.service.PinQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.presentation.pin.controller.api.PinApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PinController implements PinApi {
     private final PinCommandService pinCommandService;
+    private final PinQueryService pinQueryService;
+
+    @GetMapping("/{tripId}/pin")
+    public ResponseEntity<?> getPin(@PathVariable Long tripId) {
+        return ResponseEntity.ok()
+                .body(SuccessResponse.from(pinQueryService.getPin(tripId)));
+    }
 
     @Override
     @PostMapping("/{tripId}/pin")
