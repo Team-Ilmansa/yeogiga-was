83a1fcd6ea181b38f271ae622a00c2f4468e3cd8
DaeYoung0726 <pdy000726@naver.com>
2025-05-31 02:09:14 +0900

    feat: Silent Pust, 일반 Notification Push별 전송 메서드 분리

--- src/main/java/kr/co/yeogiga/application/fcm/service/TripPushSender.java
+++ src/main/java/kr/co/yeogiga/application/fcm/service/TripPushSender.java
@@ -1,5 +1,6 @@
 package kr.co.yeogiga.application.fcm.service;
 
+import kr.co.yeogiga.application.fcm.constant.FcmConstant;
 import kr.co.yeogiga.application.user.service.UserFcmTokenService;
 import kr.co.yeogiga.infrastructure.fcm.FcmNotificationSender;
 import kr.co.yeogiga.infrastructure.fcm.response.FcmSendResult;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -19,27 +21,64 @@ public class TripPushSender {
     private final RedisRepository redisRepository;
 
     /**
-     * FCM 토큰을 통해 푸시 알림을 비동기로 전송하는 메서드
-     * - 유효하지 않은 토큰은 Redis 및 DB에서 제거
+     * FCM 토큰을 통해 Silent Push를 비동기로 전송하는 메서드
+     * - 유효하지 않은 토큰은 Redis 및 RDB에서 제거
      *
      * @param tripId       알림 대상 여행 ID
      * @param redisListKey FCM 토큰이 저장된 Redis 리스트 키
-     * @param fcmToken     푸시 알림 전송 대상 FCM 토큰
+     * @param fcmTokens    푸시 알림 전송 대상 FCM 토큰 List
      */
     @Async
-    public void sendPush(Long tripId, String redisListKey, String fcmToken) {
+    public void sendPush(Long tripId, String redisListKey, List<String> fcmTokens) {
         Map<String, String> notificationData = buildTripPushData(tripId);
 
-        FcmSendResult result = fcmNotificationSender.send(fcmToken, notificationData);
+        for (String fcmToken : fcmTokens) {
+            FcmSendResult result = fcmNotificationSender.sendSilent(fcmToken, notificationData);
 
-        if (result.shouldRemoveToken()) {
-            redisRepository.removeFromList(redisListKey, fcmToken);
-            userFcmTokenService.deleteFcmTokenByToken(fcmToken);
+            if (result.shouldRemoveToken()) {
+                deleteFcmToken(redisListKey, fcmToken);
+            }
         }
     }
 
     /**
-     * Silent Push를 위한 data 생성 메서드
+     * FCM 토큰을 통해 Notification Push를 비동기로 전송하는 메서드
+     * - 유효하지 않은 토큰은 Redis 및 RDB에서 제거
+     *
+     * @param tripId       알림 대상 여행 ID
+     * @param title        알림 대상 여행 제목
+     * @param redisListKey FCM 토큰이 저장된 Redis 리스트 키
+     * @param fcmTokens    푸시 알림 전송 대상 FCM 토큰 List
+     */
+    @Async
+    public void sendPush(Long tripId, String title, String redisListKey, List<String> fcmTokens) {
+        Map<String, String> notificationData = buildTripPushData(tripId);
+        String pushTitle = FcmConstant.formatTitle(title);
+        String body = FcmConstant.FCM_BODY;
+
+        for (String fcmToken : fcmTokens) {
+            FcmSendResult result = fcmNotificationSender.sendNotification(fcmToken, pushTitle, body, notificationData);
+
+            if (result.shouldRemoveToken()) {
+                deleteFcmToken(redisListKey, fcmToken);
+            }
+        }
+    }
+
+    /**
+     * 유효하지 않은 FCM Token을 삭제하는 메서드
+     * - Redis 및 RDB에 저장된 FCM Token 정보를 삭제
+     *
+     * @param redisListKey FCM 토큰이 저장된 Redis 리스트 키
+     * @param fcmToken     삭제할 FCM 토큰
+     */
+    private void deleteFcmToken(String redisListKey, String fcmToken) {
+        redisRepository.removeFromList(redisListKey, fcmToken);
+        userFcmTokenService.deleteFcmTokenByToken(fcmToken);
+    }
+
+    /**
+     * Push data 생성 메서드
      *
      * @param tripId 알림 대상 여행 ID
      * @return data가 저장된 Map 자료구조
--- src/main/java/kr/co/yeogiga/infrastructure/fcm/FcmNotificationSender.java
+++ src/main/java/kr/co/yeogiga/infrastructure/fcm/FcmNotificationSender.java
@@ -7,6 +7,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MessagingErrorCode;
+import com.google.firebase.messaging.Notification;
 import kr.co.yeogiga.infrastructure.fcm.response.FcmSendResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -18,15 +19,29 @@ import java.util.Map;
 public class FcmNotificationSender {
 
     /**
+     * Silent Push 메서드
+     */
+    public FcmSendResult sendSilent(String token, Map<String, String> data) {
+        Message message = buildSilentMessage(token, data);
+        return send(token, message);
+    }
+
+    /**
+     * 일반 Notification Push 메서드
+     */
+    public FcmSendResult sendNotification(String token, String title, String body, Map<String, String> data) {
+        Message message = buildNotificationMessage(token, title, body, data);
+        return send(token, message);
+    }
+
+    /**
      * FCM 푸시 알림을 전송하고 결과를 반환하는 메서드
      *
-     * @param token 수신자의 FCM 디바이스 토큰
-     * @param data  fcm data 정보
+     * @param token   수신자의 FCM 디바이스 토큰
+     * @param message fcm message 객체
      * @return FCM 전송 결과 객체
      */
-    public FcmSendResult send(String token, Map<String, String> data) {
-        Message message = buildSilentMessage(token, data);
-
+    private FcmSendResult send(String token, Message message) {
         try {
             String response = FirebaseMessaging.getInstance().send(message);
             return FcmSendResult.success(response);
@@ -63,6 +78,29 @@ public class FcmNotificationSender {
                         .build())
                 .setAndroidConfig(AndroidConfig.builder()
                         .setPriority(AndroidConfig.Priority.HIGH)
+                        .build())
+                .build();
+    }
+
+    /**
+     * 일반 Push를 위한 메시지 생성 메서드
+     *
+     * @param token 수신자의 FCM 디바이스 토큰
+     * @param title 메시지 제목
+     * @param body  메시지 내용
+     * @param data  fcm data 정보
+     * @return FCM 전송 메시지
+     */
+    private Message buildNotificationMessage(String token, String title, String body, Map<String, String> data) {
+        return Message.builder()
+                .setToken(token)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .putAllData(data)
+                .setApnsConfig(ApnsConfig.builder()
+                        .setAps(Aps.builder().setSound("default").build())
                         .build())
                 .build();
     }
