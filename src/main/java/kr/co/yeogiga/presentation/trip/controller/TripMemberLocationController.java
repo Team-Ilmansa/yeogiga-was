c0fa32d70be3ff02bb350fc1bdd4d89549c56999
hky035 <rldud035@naver.com>
2025-05-31 03:26:32 +0900

    feat: 여행 멤버 위치 저장 rest api 작성

---
+++ src/main/java/kr/co/yeogiga/presentation/trip/controller/TripMemberLocationController.java
@@ -0,0 +1,32 @@
+package kr.co.yeogiga.presentation.trip.controller;
+
+import kr.co.yeogiga.application.trip.dto.TripMemberLocationDto;
+import kr.co.yeogiga.application.trip.service.TripMemberLocationCommandService;
+import kr.co.yeogiga.common.response.success.SuccessResponse;
+import kr.co.yeogiga.common.security.auth.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/trip")
+@RequiredArgsConstructor
+public class TripMemberLocationController {
+    private final TripMemberLocationCommandService tripMemberLocationCommandService;
+
+    @PostMapping("/{tripId}/members/location")
+    public ResponseEntity<?> saveLocation(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long tripId,
+            @RequestBody TripMemberLocationDto.Request request
+    ) {
+        tripMemberLocationCommandService.saveLocation(tripId, userDetails.getUserId(), request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.created());
+    }
+}
