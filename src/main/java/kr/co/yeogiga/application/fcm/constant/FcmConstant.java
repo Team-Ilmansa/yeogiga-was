c871b3cd134eddb8f77b6c86b5ee4fdc3269b5ab
hky035 <hky035@gmail.com>
2025-06-02 00:11:18 +0900

    feat: 집결지 요청 알림을 위한 상수 정의

--- src/main/java/kr/co/yeogiga/application/fcm/constant/FcmConstant.java
+++ src/main/java/kr/co/yeogiga/application/fcm/constant/FcmConstant.java
@@ -7,7 +7,14 @@ public final class FcmConstant {
     private static final String FCM_TITLE = "%s 여행이 시작되었습니다!";
     public static final String FCM_BODY = "앱을 열어 여행 일정을 확인해보세요.";
 
+    private static final String PIN_FCM_TITLE = "%s - 집결지 요청";
+    public static final String PIN_FCM_BODY = "앱을 열어 집결지 요청을 확인해보세요.";
+
     public static String formatTitle(String title) {
         return String.format(FCM_TITLE, title);
+    }
+
+    public static String formatPinTitle(String title) {
+        return String.format(PIN_FCM_TITLE, title);
     }
 }
