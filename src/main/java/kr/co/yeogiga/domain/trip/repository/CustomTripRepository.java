98d5be4f07ea8aa8b6489c5df2c7b9d8586e3cb7
hky035 <hky035@gmail.com>
2025-06-03 21:34:25 +0900

    refact: TripRepository 내 JPQL 사용 쿼리 메서드 QueryDsl 마이그레이션

--- src/main/java/kr/co/yeogiga/domain/trip/repository/CustomTripRepository.java
+++ src/main/java/kr/co/yeogiga/domain/trip/repository/CustomTripRepository.java
@@ -5,9 +5,16 @@ import kr.co.yeogiga.domain.trip.dto.TripFcmTokenQueryDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomTripRepository {
     List<TripFcmTokenQueryDto> findTripFcmTokensByTime(LocalDateTime time);
 
     List<TripFcmTokenInfoDto> findTripFcmTokenInfosById(Long tripId);
+
+    Optional<Long> findLeaderIdById(Long tripId);
+
+    void updateTravelStatusInProgress(LocalDateTime time);
+
+    void updateTravelStatusCompleted(LocalDateTime time);
 }
--- src/main/java/kr/co/yeogiga/domain/trip/repository/TripRepository.java
+++ src/main/java/kr/co/yeogiga/domain/trip/repository/TripRepository.java
@@ -2,31 +2,11 @@ package kr.co.yeogiga.domain.trip.repository;
 
 import kr.co.yeogiga.domain.trip.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface TripRepository extends JpaRepository<Trip, Long>, CustomTripRepository {
 
-    @Modifying
-    @Query("UPDATE trip " +
-            "SET travelStatus = 'IN_PROGRESS' " +
-            "WHERE travelStatus != 'IN_PROGRESS' AND travelStatus != 'SETTING' AND startedAt <= :time AND :time <= endedAt")
-    void updateTravelStatusInProgress(@Param(value = "time") LocalDateTime time);
-
-    @Modifying
-    @Query("UPDATE trip " +
-            "SET travelStatus = 'COMPLETED' " +
-            "WHERE travelStatus != 'COMPLETED' AND travelStatus != 'SETTING' AND endedAt <= :time")
-    void updateTravelStatusCompleted(@Param(value = "time") LocalDateTime time);
-
     List<Trip> findAllByIdIn(Set<Long> ids);
-
-    @Query("SELECT t.leaderId FROM trip t WHERE t.id = :tripId")
-    Optional<Long> findLeaderIdById(@Param(value = "tripId") Long tripId);
 }
