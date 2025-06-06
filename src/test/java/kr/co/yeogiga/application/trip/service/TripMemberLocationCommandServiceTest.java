d26ba050c0b404d03f8a11d52727382d57358fe2
DaeYoung0726 <pdy000726@naver.com>
2025-06-06 15:42:38 +0900

    test: 사용자 GPS 저장 과정 중, 권한에 따른 분기 처리 test code 작성

--- src/test/java/kr/co/yeogiga/application/trip/service/TripMemberLocationCommandServiceTest.java
+++ src/test/java/kr/co/yeogiga/application/trip/service/TripMemberLocationCommandServiceTest.java
@@ -1,10 +1,14 @@
 package kr.co.yeogiga.application.trip.service;
 
+import kr.co.yeogiga.application.route.service.TripLeaderCommandService;
 import kr.co.yeogiga.application.trip.dto.TripMemberLocationDto;
 import kr.co.yeogiga.common.exception.CustomException;
+import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.domain.trip.exception.TripMemberErrorType;
 import kr.co.yeogiga.domain.trip.service.TripMemberService;
+import kr.co.yeogiga.domain.trip.service.TripService;
 import kr.co.yeogiga.infrastructure.redis.RedisRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,14 +18,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,6 +37,12 @@ public class TripMemberLocationCommandServiceTest {
 
     @Mock
     private TripMemberService tripMemberService;
+
+    @Mock
+    private TripService tripService;
+
+    @Mock
+    private TripLeaderCommandService tripLeaderCommandService;
 
     @Mock
     private RedisRepository redisRepository;
@@ -43,10 +55,20 @@ public class TripMemberLocationCommandServiceTest {
     class SaveLocation {
         private final Long tripId = 1L;
         private final Long userId = 1L;
+        private final Trip trip = Trip.builder()
+                .leaderId(userId)
+                .build();
+
+        @BeforeEach
+        void setUp() {
+            LocalDateTime start = LocalDateTime.of(2025, 6, 1, 0, 0);
+            LocalDateTime end = LocalDateTime.of(2025, 6, 5, 0, 0);
+            trip.updateTime(start, end);
+        }
 
         @Test
-        @DisplayName("성공")
-        void success() {
+        @DisplayName("성공 - 방장")
+        void successIsLeader() {
             // given
             TripMemberLocationDto.Request request
                     = new TripMemberLocationDto.Request(1.1, 2.2);
@@ -54,6 +76,7 @@ public class TripMemberLocationCommandServiceTest {
             when(tripMemberService.existsByTripIdAndUserId(tripId, userId)).thenReturn(true);
             doNothing().when(redisRepository).setHash(anyString(), anyString(), any());
             doNothing().when(redisRepository).setHashExpire(anyString(), anyString(), any());
+            when(tripService.readById(tripId)).thenReturn(Optional.ofNullable(trip));
 
             // when
             tripMemberLocationCommandService.saveLocation(tripId, userId, request);
@@ -61,6 +84,29 @@ public class TripMemberLocationCommandServiceTest {
             // then
             verify(redisRepository, times(1)).setHash(isA(String.class), isA(String.class), isA(TripMemberLocationDto.StoredFormat.class));
             verify(redisRepository, times(1)).setHashExpire(isA(String.class), isA(String.class), isA(Duration.class));
+            verify(tripLeaderCommandService, times(1)).storeLeaderRouteInRedis(isA(Long.class), isA(Integer.class), isA(Double.class), isA(Double.class));
+        }
+
+        @Test
+        @DisplayName("성공 - 방장 아님")
+        void successIsNotLeader() {
+            // given
+            TripMemberLocationDto.Request request
+                    = new TripMemberLocationDto.Request(1.1, 2.2);
+            Long memberId = 2L;
+
+            when(tripMemberService.existsByTripIdAndUserId(tripId, memberId)).thenReturn(true);
+            doNothing().when(redisRepository).setHash(anyString(), anyString(), any());
+            doNothing().when(redisRepository).setHashExpire(anyString(), anyString(), any());
+            when(tripService.readById(tripId)).thenReturn(Optional.ofNullable(trip));
+
+            // when
+            tripMemberLocationCommandService.saveLocation(tripId, memberId, request);
+
+            // then
+            verify(redisRepository, times(1)).setHash(isA(String.class), isA(String.class), isA(TripMemberLocationDto.StoredFormat.class));
+            verify(redisRepository, times(1)).setHashExpire(isA(String.class), isA(String.class), isA(Duration.class));
+            verify(tripLeaderCommandService, never()).storeLeaderRouteInRedis(isA(Long.class), isA(Integer.class), isA(Double.class), isA(Double.class));
         }
 
         @Test
