82fe1761652c9407d016fbf1b51dfaa31acda042
hky035 <rldud035@naver.com>
2025-06-06 10:57:23 +0900

    feat: UserRepository querydsl 마이그레이션
     * UserRepository 내 Spring-Data-JPA 네이티브 쿼리 메서드 -> querydsl 네이티브 쿼리 마이그레이션

--- src/main/java/kr/co/yeogiga/domain/user/repository/UserRepository.java
+++ src/main/java/kr/co/yeogiga/domain/user/repository/UserRepository.java
@@ -1,66 +1,13 @@
 package kr.co.yeogiga.domain.user.repository;
 
-import kr.co.yeogiga.domain.oauth.type.OAuthPlatform;
 import kr.co.yeogiga.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query(value = "SELECT u.id, u.username, u.password, u.email, u.nickname,  u.image_url, u.fcm_token, u.signed_up, u.role, u.deleted_at, u.created_at, u.modified_at " +
-                   "FROM users u " +
-                   "INNER JOIN oauth o ON u.id = o.user_id " +
-                   "WHERE o.platform = :platform AND o.platform_id = :platformId", nativeQuery = true)
-    Optional<User> findUserIncludeDeletedByPlatformAndPlatformId(@Param(value = "platform") String platform, @Param("platformId") String platformId);
-
-    @Query(value = "SELECT * " +
-                   "FROM users " +
-                   "WHERE id = :id", nativeQuery = true)
-    Optional<User> findUserIncludeDeletedById(@Param(value = "id") Long id);
-
-    @Query(value = "SELECT * " +
-            "FROM users " +
-            "WHERE nickname = :nickname", nativeQuery = true)
-    Optional<User> findUserIncludeDeletedByNickname(String nickname);
-
-    @Query(value = "SELECT * " +
-            "FROM users " +
-            "WHERE username = :username", nativeQuery = true)
-    Optional<User> findUserIncludeDeletedByUsername(@Param(value = "username") String username);
-
-    @Query(value = "SELECT id " +
-                   "FROM users " +
-                   "WHERE deleted_at IS NOT NULL AND deleted_at <= :date", nativeQuery = true)
-    List<Long> findDeletedUserIdBefore(@Param(value = "date") LocalDate date);
-
-    @Query(value = "SELECT id " +
-                   "FROM users " +
-                   "WHERE username = :username LIMIT 1", nativeQuery = true)
-    Optional<Long> findUserIdIncludeDeletedByUsername(@Param(value = "username") String username);
-
-    @Query(value = "SELECT id " +
-                   "FROM users " +
-                   "WHERE nickname = :nickname LIMIT 1", nativeQuery = true)
-    Optional<Long> findUserIdIncludeDeletedByNickname(@Param(value = "nickname") String nickname);
-
-    @Query(value = "SELECT id " +
-            "FROM users " +
-            "WHERE email = :email LIMIT 1", nativeQuery = true)
-    Optional<Long> findUserIdIncludeDeletedByEmail(@Param(value = "email") String email);
-
+public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
     Optional<User> findByFcmToken(String fcmToken);
     
     List<User> findAllByIdIn(List<Long> ids);
-
-    @Modifying
-    @Query(value = "DELETE " +
-            "FROM users " +
-            "WHERE id IN :ids", nativeQuery = true)
-    void deleteHardAllByIds(List<Long> ids);
 }
