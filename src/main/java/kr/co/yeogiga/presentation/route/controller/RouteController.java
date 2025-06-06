699417fcb331f376a4a84088a04ac7e132fba8f3
DaeYoung0726 <pdy000726@naver.com>
2025-06-06 15:45:26 +0900

    refact: 방장 GPS 저장 rest api 삭제

--- src/main/java/kr/co/yeogiga/presentation/route/controller/RouteController.java
+++ src/main/java/kr/co/yeogiga/presentation/route/controller/RouteController.java
@@ -1,17 +1,12 @@
 package kr.co.yeogiga.presentation.route.controller;
 
-import kr.co.yeogiga.application.route.dto.RouteReq;
-import kr.co.yeogiga.application.route.service.TripLeaderCommandService;
 import kr.co.yeogiga.application.route.service.TripRouteQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.presentation.route.api.RouteApi;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,19 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/trip")
 @RequiredArgsConstructor
 public class RouteController implements RouteApi {
-    private final TripLeaderCommandService tripLeaderCommandService;
     private final TripRouteQueryService tripRouteQueryService;
-
-    @Override
-    @PostMapping("/{tripId}/days/{day}/routes")
-    public ResponseEntity<?> storeLeaderRoute(
-            @PathVariable Long tripId,
-            @PathVariable int day,
-            @RequestBody RouteReq.Request routeReq
-    ) {
-        tripLeaderCommandService.storeLeaderRouteInRedis(tripId, day, routeReq);
-        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.created());
-    }
 
     @Override
     @GetMapping("/{tripId}/routes")
