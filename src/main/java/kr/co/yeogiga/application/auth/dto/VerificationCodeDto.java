fdedeae7c4297fd89d2f40bd0cd62d4a12663916
hky035 <rldud035@naver.com>
2025-07-05 18:05:30 +0900

    feat: 인증 코드 검증 요청 dto 추가

--- src/main/java/kr/co/yeogiga/application/auth/dto/VerificationCodeDto.java
+++ src/main/java/kr/co/yeogiga/application/auth/dto/VerificationCodeDto.java
@@ -10,4 +10,13 @@ public class VerificationCodeDto {
             @Pattern(regexp = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "잘못된 이메일 형식입니다.")
             String email
     ) { }
+    
+    public record VerificationDto(
+            @NotBlank(message = "이메일은 필수 입력값입니다.")
+            @Pattern(regexp = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "잘못된 이메일 형식입니다.")
+            String email,
+            
+            @NotBlank(message = "인증 코드는 필수 입력값입니다.")
+            String code
+    ) { }
 }
