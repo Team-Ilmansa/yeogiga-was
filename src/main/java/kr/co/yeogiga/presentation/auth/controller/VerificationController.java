17697e539b2e9eaa149bd76e3810ce0d3260f333
hky035 <rldud035@naver.com>
2025-07-05 18:06:41 +0900

    feat: 이메일 인증 코드 검증 api 구현

--- src/main/java/kr/co/yeogiga/presentation/auth/controller/VerificationController.java
+++ src/main/java/kr/co/yeogiga/presentation/auth/controller/VerificationController.java
@@ -5,6 +5,7 @@ import kr.co.yeogiga.application.auth.dto.VerificationCodeDto;
 import kr.co.yeogiga.application.auth.service.VerificationCodeService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,5 +22,14 @@ public class VerificationController {
     public ResponseEntity<?> sendEmailVerificationCode(@Valid @RequestBody VerificationCodeDto.SendRequest request) {
         verificationCodeService.issueCode(request.email());
         return ResponseEntity.ok(SuccessResponse.ok());
+    }
+    
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyEmailVerificationCode(@Valid @RequestBody VerificationCodeDto.VerificationDto request) {
+        verificationCodeService.verifyCode(request.email(), request.code());
+        return ResponseEntity.ok(SuccessResponse.builder()
+                        .code(HttpStatus.OK.value())
+                        .message("인증에 성공하였습니다.")
+                        .build());
     }
 }
