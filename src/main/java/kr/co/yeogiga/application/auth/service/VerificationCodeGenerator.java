8e0438e9f9667664c44f69da6472ac981b1223ec
hky035 <rldud035@naver.com>
2025-07-05 18:57:48 +0900

    docs: 이메일 인증 관련 로직 주석 작성

--- src/main/java/kr/co/yeogiga/application/auth/service/VerificationCodeGenerator.java
+++ src/main/java/kr/co/yeogiga/application/auth/service/VerificationCodeGenerator.java
@@ -8,6 +8,12 @@ import java.security.SecureRandom;
 public class VerificationCodeGenerator {
     private final SecureRandom secureRandom = new SecureRandom();
     
+    /**
+     * 6자리 난수를 생성하는 메서드
+     * - 범위: 100,000 ~ 999,999
+     *
+     * @return 6자리 난수
+     */
     public String generate() {
         return String.valueOf(secureRandom.nextInt(900000) + 100000);
     }
--- src/main/java/kr/co/yeogiga/application/auth/service/VerificationCodeService.java
+++ src/main/java/kr/co/yeogiga/application/auth/service/VerificationCodeService.java
@@ -13,7 +13,12 @@ public class VerificationCodeService {
     private final VerificationCodeCache verificationCodeCache;
     private final VerificationCodeGenerator verificationCodeGenerator;
     private final VerificationCodeEmailSender verificationCodeEmailSender;
-
+    
+    /**
+     * 사용자가 인증 요청한 이메일로 인증 번호를 발송하는 메서드
+     *
+     * @param email 인증 요청 이메일
+     */
     public void issueCode(String email) {
         String code = verificationCodeGenerator.generate();
         
@@ -22,6 +27,14 @@ public class VerificationCodeService {
         verificationCodeEmailSender.send(email, code);
     }
     
+    /**
+     * 사용자가 요청한 이메일 인증 번호를 검증하는 메서드
+     *
+     * @param email 사용자 인증 요청 이메일
+     * @param code  사용자가 요청한 이메일 인증 번호
+     * @throws CustomException AuthErrorType.EMAIL_VERIFICATION_TIMEOUT - 저장소 내 이메일 존재하지 않거나 인증 시간이 초과된 경우
+     * @throws CustomException AuthErrorType.EMAIL_VERIFICATION_CODE_MISMATCH - 이메일 인증 번호 불일치
+     */
     public void verifyCode(String email, String code) {
         String verifiedCode = verificationCodeCache.getCode(email)
                 .orElseThrow(() -> new CustomException(AuthErrorType.EMAIL_VERIFICATION_TIMEOUT));
