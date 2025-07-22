513befefe4d87da56d969cbc92c48509690420c3
DaeYoung0726 <pdy000726@naver.com>
2025-07-22 17:12:41 +0900

    refact: 목적지 쓰기 요청 dto 카테고리 타입 변경 * 중복 DTO 삭제 (InsertRequest)

--- src/main/java/kr/co/yeogiga/application/tripplace/dto/TripPlaceReq.java
+++ src/main/java/kr/co/yeogiga/application/tripplace/dto/TripPlaceReq.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class TripPlaceReq {
 
     @Builder
-    @Schema(name = "TripPlaceDto.Request", description = "여행 목적지 추가 DTO")
+    @Schema(name = "TripPlaceReq.Request", description = "여행 목적지 추가 DTO")
     public record Request(
             @Schema(description = "목적지 이름", example = "광화문")
             String name,
@@ -19,16 +19,27 @@ public class TripPlaceReq {
             double latitude,
             @Schema(description = "목적지 경도", example = "123.123")
             double longitude,
-            @Schema(description = "목적지 타입(카테고리)", example = "관광명소")
-            String placeType
+            @Schema(description = "목적지 타입(카테고리)", example = "RESTAURANT, TOURISM, LODGING, ETC")
+            PlaceCategory placeType
     ) {
+        public Place toEntity(Double order) {
+            return Place.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name(name)
+                    .latitude(latitude)
+                    .longitude(longitude)
+                    .order(order)
+                    .placeType(placeType)
+                    .build();
+        }
+
         public StoredFormat toStoredFormat() {
             return new StoredFormat(
                     UUID.randomUUID().toString(),
                     name,
                     latitude,
                     longitude,
-                    PlaceCategory.fromLabel(placeType).getGroupName()
+                    placeType
             );
         }
     }
@@ -38,40 +49,16 @@ public class TripPlaceReq {
             String name,
             double latitude,
             double longitude,
-            String placeCategory
+            PlaceCategory placeCategory
     ) { }
 
-    @Schema(name = "TripPlaceDto.CompleteRequest", description = "여행 목적지 선택 완료 요청 DTO")
+    @Schema(name = "TripPlaceReq.CompleteRequest", description = "여행 목적지 선택 완료 요청 DTO")
     public record CompleteRequest(
             @Schema(description = "편집 완료된 마지막 일차", example = "5")
             int lastDay
     ) { }
 
-    @Builder
-    @Schema(name = "TripPlaceDto.InsertRequest", description = "삽입 위치를 포함한 새로운 목적지 추가 요청 DTO")
-    public record InsertRequest(
-            @Schema(description = "목적지 이름", example = "광화문")
-            String name,
-            @Schema(description = "위도", example = "37.57")
-            double latitude,
-            @Schema(description = "경도", example = "126.98")
-            double longitude,
-            @Schema(description = "장소 타입", example = "관광명소")
-            String placeType
-    ) {
-        public Place toEntity(Double order) {
-            return Place.builder()
-                    .id(UUID.randomUUID().toString())
-                    .name(name)
-                    .latitude(latitude)
-                    .longitude(longitude)
-                    .order(order)
-                    .placeType(PlaceCategory.fromLabel(placeType).getGroupName())
-                    .build();
-        }
-    }
-
-    @Schema(name = "TripPlaceDto.ReorderRequest", description = "여행 목적지 순서 변경 요청 DTO")
+    @Schema(name = "TripPlaceReq.ReorderRequest", description = "여행 목적지 순서 변경 요청 DTO")
     public record ReorderRequest(
             @Schema(description = "정렬된 목적지 ID 리스트", example = "[\"place3-id\", \"place1-id\", \"place2-id\"]")
             List<String> orderedPlaceIds
