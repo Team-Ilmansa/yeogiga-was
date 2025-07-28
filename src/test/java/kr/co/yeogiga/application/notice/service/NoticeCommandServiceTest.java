d2b62a3a01aee2ad532d7b09caf69b4f4d64a079
hky035 <rldud035@naver.com>
2025-07-28 19:05:25 +0900

    test: 공지사항 조회 로직 테스트 코드 작성

--- src/test/java/kr/co/yeogiga/application/notice/service/NoticeCommandServiceTest.java
+++ src/test/java/kr/co/yeogiga/application/notice/service/NoticeCommandServiceTest.java
@@ -1,8 +1,12 @@
 package kr.co.yeogiga.application.notice.service;
 
 import kr.co.yeogiga.application.notice.dto.NoticeReq;
+import kr.co.yeogiga.common.exception.CustomException;
 import kr.co.yeogiga.domain.notice.entity.Notice;
+import kr.co.yeogiga.domain.notice.exception.NoticeErrorType;
 import kr.co.yeogiga.domain.notice.service.NoticeService;
+import kr.co.yeogiga.domain.user.entity.User;
+import kr.co.yeogiga.domain.user.type.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,12 +15,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.assertArg;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class NoticeCommandServiceTest {
@@ -56,4 +65,67 @@ public class NoticeCommandServiceTest {
         }
     }
     
+    @Nested
+    @DisplayName("공지사항 수정")
+    class UpdateNotice {
+        User user = User.builder()
+                .id(1L)
+                .nickname("nickname")
+                .role(Role.USER)
+                .build();
+        
+        Notice notice = Notice.builder()
+                .tripId(2L)
+                .title("title")
+                .description("description")
+                .author(user)
+                .build();
+        
+        NoticeReq.Creation dto = NoticeReq.Creation.builder()
+                .title("new title")
+                .description("new description")
+                .build();
+        
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            when(noticeService.readByIdJoinUser(2L)).thenReturn(Optional.of(notice));
+            
+            // when
+            noticeCommandService.updateNotice(2L, 1L, dto);
+            
+            // when
+            assertEquals(dto.title(), notice.getTitle());
+            assertEquals(dto.description(), notice.getDescription());
+        }
+        
+        @Test
+        @DisplayName("실패 - 존재하지 않는 공지사항")
+        void failIfNoticeNotFound() {
+            // given
+            when(noticeService.readByIdJoinUser(anyLong())).thenReturn(Optional.empty());
+            
+            // when
+            CustomException exception = assertThrows(CustomException.class, ()
+                    -> noticeCommandService.updateNotice(2L, 1L, dto));
+            
+            // then
+            assertEquals(NoticeErrorType.NOT_FOUND, exception.getErrorType());
+        }
+        
+        @Test
+        @DisplayName("실패 - 작성자가 아닌 경우")
+        void failUnauthorizedAuthor() {
+            // given
+            when(noticeService.readByIdJoinUser(anyLong())).thenReturn(Optional.of(notice));
+            
+            // when
+            CustomException exception = assertThrows(CustomException.class, ()
+                    -> noticeCommandService.updateNotice(2L, 3L, dto));
+            
+            // then
+            assertEquals(NoticeErrorType.UNAUTHORIZED_AUTHOR, exception.getErrorType());
+        }
+    }
 }
