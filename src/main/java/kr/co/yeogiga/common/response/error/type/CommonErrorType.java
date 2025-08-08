57ad35ba1ac12b44c9e4c2a663c43f5e9235731f
hky035 <rldud035@naver.com>
2025-08-08 23:27:56 +0900

    feat: MethodArgumentTypeMismatchException 예외 처리 핸들러 메서드 로직 변경
     * 메서드 어노테이션 타입에 따른 Path Variable, Query String 값 에러 응답 처리 분리

--- src/main/java/kr/co/yeogiga/common/response/error/type/CommonErrorType.java
+++ src/main/java/kr/co/yeogiga/common/response/error/type/CommonErrorType.java
@@ -11,7 +11,8 @@ public enum CommonErrorType implements BaseErrorType {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G001", "서버 내부 에러입니다."),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G002", "유효성 검증에 실패하였습니다."),
     PATH_VARIABLE_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G003", "지원하지 않는 Path Variable 값입니다."),
-    TIME_SHOULD_NOT_BEFORE_NOW(HttpStatus.BAD_REQUEST, "G004", "요청 시각은 현재 시각 이전이 될 수 없습니다.");
+    TIME_SHOULD_NOT_BEFORE_NOW(HttpStatus.BAD_REQUEST, "G004", "요청 시각은 현재 시각 이전이 될 수 없습니다."),
+    QUERY_STRING_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G005", "지원하지 않는 Query String 값입니다.");
 
     private final HttpStatus status;
     private final String code;
