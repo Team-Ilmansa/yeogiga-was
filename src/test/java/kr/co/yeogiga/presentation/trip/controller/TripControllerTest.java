5dafa2db1fd2fd9eeef322fc52d331e0a23a13f8
hky035 <rldud035@naver.com>
2025-08-14 13:14:34 +0900

    test: 페이지네이션 적용으로 인한 여행 전체 조회 api 테스트 코드 변경

--- src/test/java/kr/co/yeogiga/presentation/trip/controller/TripControllerTest.java
+++ src/test/java/kr/co/yeogiga/presentation/trip/controller/TripControllerTest.java
@@ -31,6 +31,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -379,7 +381,7 @@ public class TripControllerTest {
                                     .build()))
                     .build();
 
-            when(tripQueryService.getAllTrip(anyLong(), any(TripStatus.class))).thenReturn(List.of(tripSummary));
+            when(tripQueryService.getAllTrip(anyLong(), any(TripStatus.class), any(Pageable.class))).thenReturn(new PageImpl<>(List.of(tripSummary)));
 
             // when
             ResultActions resultActions = mockMvc.perform(
