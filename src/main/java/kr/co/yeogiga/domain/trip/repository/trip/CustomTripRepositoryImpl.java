be4fc447ae383e3cf12217496b4917a98fe8a6be
DaeYoung0726 <pdy000726@naver.com>
2025-08-19 03:18:02 +0900

    chore: 여행 레포지토리 패키지 위치 변경

--- src/main/java/kr/co/yeogiga/domain/trip/repository/trip/CustomTripRepositoryImpl.java
+++ src/main/java/kr/co/yeogiga/domain/trip/repository/trip/CustomTripRepositoryImpl.java
@@ -1,4 +1,4 @@
-package kr.co.yeogiga.domain.trip.repository;
+package kr.co.yeogiga.domain.trip.repository.trip;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
