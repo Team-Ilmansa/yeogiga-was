35e9cd06b30bb29435858a827a1d844d9e447150
DaeYoung0726 <pdy000726@naver.com>
2025-08-19 01:13:39 +0900

    feat: 여행 목적지 Entity 생성

---
+++ src/main/java/kr/co/yeogiga/domain/trip/entity/Place.java
@@ -0,0 +1,57 @@
+package kr.co.yeogiga.domain.trip.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.yeogiga.domain.trip.type.PlaceCategory;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "place")
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private Double order;
+
+    @Column(name = "is_visited")
+    private boolean isVisited;
+
+    @Column(name = "place_type")
+    @Enumerated(EnumType.STRING)
+    private PlaceCategory placeType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trip_day_id")
+    private TripDay tripDay;
+
+    @Builder
+    public Place(String name, Double latitude, Double longitude,
+                 PlaceCategory placeType, Double order, TripDay tripDay) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.order = order;
+        this.isVisited = false;
+        this.placeType = placeType;
+        this.tripDay = tripDay;
+    }
+}
