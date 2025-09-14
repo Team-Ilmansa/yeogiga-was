bcfc70c1d8a10dbfdfd7eda2b49a7ac6e66ac4c4
hky035 <rldud035@naver.com>
2025-09-15 01:47:21 +0900

    feat: 정산 내역 조회 api 구현
     * RESTful API 명명 규칙 준수를 위한 엔드포인트 변경

--- src/main/java/kr/co/yeogiga/presentation/settlement/controller/SettlementController.java
+++ src/main/java/kr/co/yeogiga/presentation/settlement/controller/SettlementController.java
@@ -3,6 +3,7 @@ package kr.co.yeogiga.presentation.settlement.controller;
 import jakarta.validation.Valid;
 import kr.co.yeogiga.application.settlement.dto.SettlementRequest;
 import kr.co.yeogiga.application.settlement.service.SettlementCommandService;
+import kr.co.yeogiga.application.settlement.service.SettlementQueryService;
 import kr.co.yeogiga.common.response.success.SuccessResponse;
 import kr.co.yeogiga.common.security.auth.CustomUserDetailsImpl;
 import kr.co.yeogiga.presentation.settlement.api.SettlementApi;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/settlement")
+@RequestMapping("/api/v1/trip")
 @RequiredArgsConstructor
 public class SettlementController implements SettlementApi {
     private final SettlementCommandService settlementCommandService;
+    private final SettlementQueryService settlementQueryService;
     
     @Override
-    @PostMapping("/{tripId}")
+    @PostMapping("/{tripId}/settlements")
     public ResponseEntity<?> createSettlement(
             @PathVariable(name = "tripId") Long tripId,
             @AuthenticationPrincipal CustomUserDetailsImpl userDetails,
@@ -32,5 +35,15 @@ public class SettlementController implements SettlementApi {
         settlementCommandService.createSettlement(tripId, userDetails.getUserId(), settlement);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(SuccessResponse.created());
+    }
+    
+    @GetMapping("/{tripId}/settlements/{settlementId}")
+    public ResponseEntity<?> getSettlement(
+            @AuthenticationPrincipal CustomUserDetailsImpl userDetails,
+            @PathVariable(name ="tripId") Long tripId,
+            @PathVariable(name = "settlementId") Long settlementId
+    ) {
+        return ResponseEntity
+                .ok(settlementQueryService.getSettlement(tripId, userDetails.getUserId(), settlementId));
     }
 }
