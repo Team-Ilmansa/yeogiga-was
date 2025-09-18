77524399d3be1b16cc7a6485ca31c196cf3f8a02
hky035 <rldud035@naver.com>
2025-09-19 03:11:24 +0900

    feat: 정산 내역 전체 조회 로직 구현

--- src/main/java/kr/co/yeogiga/application/settlement/service/SettlementQueryService.java
+++ src/main/java/kr/co/yeogiga/application/settlement/service/SettlementQueryService.java
@@ -9,6 +9,11 @@ import kr.co.yeogiga.domain.trip.service.TripMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class SettlementQueryService {
@@ -35,4 +40,22 @@ public class SettlementQueryService {
                 .orElseThrow(() -> new CustomException(SettlementErrorType.NOT_FOUND));
     }
     
+    /**
+     * 전체 정산 내역 목록을 조회하는 메서드
+     *
+     * @param tripId    여행 ID
+     * @param userId    사용자 ID
+     * @return          날짜를 Key, 해당 날짜의 정산 내역 목록을 Value로 가지는 정산 내역 목록 Map
+     *
+     * @throws CustomException TripMemberErrorType.IS_NOT_MEMBER - 여행 멤버가 아닌 사용자가 요청을 보낸 경우
+     */
+    public Map<LocalDate, List<SettlementDto>> getAllSettlement(Long tripId, Long userId) {
+        if (!tripMemberService.existsByTripIdAndUserId(tripId, userId)) {
+            throw new CustomException(TripMemberErrorType.IS_NOT_MEMBER);
+        }
+        
+        List<SettlementDto> settlements = settlementService.readAllSettlementDtoByTripId(tripId);
+        
+        return settlements.stream().collect(Collectors.groupingBy(SettlementDto::date));
+    }
 }
