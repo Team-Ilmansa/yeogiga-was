475f127516481dedc1ca3ba20e48085ef5864950
hky035 <rldud035@naver.com>
2025-10-17 03:25:36 +0900

    feat: 정산 완료 갱신 로직 구현

--- src/main/java/kr/co/yeogiga/domain/settlement/exception/SettlementErrorType.java
+++ src/main/java/kr/co/yeogiga/domain/settlement/exception/SettlementErrorType.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum SettlementErrorType implements BaseErrorType {
     NOT_VALID_PRICE(HttpStatus.BAD_REQUEST, "S000", "정산 내역 금액 총합이 일치하지 않습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "S001", "존재하지 않는 정산 내역 입니다."),
-    IS_NOT_PAYER(HttpStatus.FORBIDDEN, "S002", "정산자가 아닙니다.");
+    IS_NOT_PAYER(HttpStatus.FORBIDDEN, "S002", "정산자가 아닙니다."),
+    PAY_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "S003", "존재하지 않는 분담 내역입니다.");
     
     private final HttpStatus status;
     private final String code;
