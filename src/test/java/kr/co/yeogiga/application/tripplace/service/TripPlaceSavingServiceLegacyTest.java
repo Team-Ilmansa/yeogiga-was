c622fb1fd68d93ef5a774b6c271a607fa73d9dd2
hky035 <rldud035@naver.com>
2025-10-25 02:33:52 +0900

    test: 여행 목적지 확정 기능 내 여행 주 목적지(지역) 할당 로직 test code 작성

--- src/test/java/kr/co/yeogiga/application/tripplace/service/TripPlaceSavingServiceLegacyTest.java
+++ src/test/java/kr/co/yeogiga/application/tripplace/service/TripPlaceSavingServiceLegacyTest.java
@@ -3,9 +3,9 @@ package kr.co.yeogiga.application.tripplace.service;
 import kr.co.yeogiga.application.tripplace.dto.TripPlaceReqLegacy;
 import kr.co.yeogiga.domain.trip.entity.Trip;
 import kr.co.yeogiga.domain.trip.service.TripService;
+import kr.co.yeogiga.domain.trip.type.PlaceCategory;
 import kr.co.yeogiga.domain.trip.type.TravelStatus;
 import kr.co.yeogiga.domain.tripplace.service.TripDayPlaceService;
-import kr.co.yeogiga.domain.trip.type.PlaceCategory;
 import kr.co.yeogiga.infrastructure.redis.RedisRepository;
 import kr.co.yeogiga.infrastructure.redis.constant.PlaceConstant;
 import org.junit.jupiter.api.DisplayName;
@@ -25,6 +25,7 @@ import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -180,6 +181,50 @@ public class TripPlaceSavingServiceLegacyTest {
             verify(redisRepository, times(1)).del(PlaceConstant.dayPlaceSetKey(tripId, 2));
 
             assertEquals(TravelStatus.COMPLETED, trip.getTravelStatus());
+        }
+    }
+    
+    @Test
+    @DisplayName("여행 목적지 확정 성공 - 여행 주 목적지(지역) 할당 테스트")
+    void successAssignTripCity() {
+        // given
+        TripPlaceReqLegacy.StoredFormat place1 = new TripPlaceReqLegacy.StoredFormat(
+                "id1", "달성공원", "대구광역시 중구 달성공원로35", 33.123, 126.456, PlaceCategory.TOURISM
+        );
+        TripPlaceReqLegacy.StoredFormat place2 = new TripPlaceReqLegacy.StoredFormat(
+                "id2", "포함시청", "경상북도 포항시 남구 대잠동 1001 포항시청", 33.789, 126.987, PlaceCategory.TOURISM
+        );
+        TripPlaceReqLegacy.StoredFormat place3 = new TripPlaceReqLegacy.StoredFormat(
+                "id3", "포항역 고속철", "경상북도 포항시 북구 흥해읍 이인리 137-1", 33.789, 126.987, PlaceCategory.TRANSPORT
+        );
+        TripPlaceReqLegacy.StoredFormat place4 = new TripPlaceReqLegacy.StoredFormat(
+                "id4", "한라산", "제주특별자치도 서귀포시 토평동 산15-1", 33.789, 126.987, PlaceCategory.TOURISM
+        );
+        
+        when(redisRepository.getList(PlaceConstant.dayPlacesKey(tripId, 1), TripPlaceReqLegacy.StoredFormat.class))
+                .thenReturn(List.of(place1, place2));
+                
+        
+        when(redisRepository.getList(PlaceConstant.dayPlacesKey(tripId, 2), TripPlaceReqLegacy.StoredFormat.class))
+                .thenReturn(List.of(place3, place4));
+        
+        ReflectionTestUtils.setField(trip, "startedAt", LocalDateTime.of(2025, 5, 20, 12, 0));
+        ReflectionTestUtils.setField(trip, "endedAt", LocalDateTime.of(2025, 5, 21, 12, 0));
+        
+        when(tripService.readById(tripId)).thenReturn(Optional.of(trip));
+        
+        try (MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class, Mockito.CALLS_REAL_METHODS)) {
+            String date = "2025-05-24T12:00:00Z";
+            Clock clock = Clock.fixed(Instant.parse(date), ZoneId.of("UTC"));
+            LocalDateTime mockNow = LocalDateTime.now(clock);
+            mockedLocalDateTime.when(LocalDateTime::now).thenReturn(mockNow);
+            
+            // when
+            tripPlaceSavingServiceLegacy.completeTrip(tripId, 2);
+            
+            // then: 여행 목적지(지역)은 중복인 목적지로 인하여 총 3곳이며, "대구광역시", "포항시", "제주특별자치도"이다.
+            assertThat(trip.getCity()).hasSize(3);
+            assertThat(trip.getCity()).containsAll(List.of("대구광역시", "포항시", "제주특별자치도"));
         }
     }
 }
