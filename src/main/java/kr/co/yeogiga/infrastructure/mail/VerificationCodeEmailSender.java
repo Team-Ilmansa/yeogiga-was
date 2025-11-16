306b2ad62dbe0dd1ec73a071bfd2cb3156aa29e2
hky035 <rldud035@naver.com>
2025-11-16 15:55:50 +0900

    feat: 이메일 인증 코드 발송 로직 내 템플릿 적용

--- src/main/java/kr/co/yeogiga/infrastructure/mail/VerificationCodeEmailSender.java
+++ src/main/java/kr/co/yeogiga/infrastructure/mail/VerificationCodeEmailSender.java
@@ -2,20 +2,28 @@ package kr.co.yeogiga.infrastructure.mail;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
 @RequiredArgsConstructor
 public class VerificationCodeEmailSender {
     private final JavaEmailSender javaEmailSender;
+    private final TemplateEngine templateEngine;
+    
     private final String SUBJECT = "[여기가] 이메일 인증 코드";
-    // TODO: thymeleaf 템플릿 엔진을 통한 이메일 인증 코드 메일 템플릿 추가 시 삭제 필요
-    private final String CONTENT = """
-                <h1>여기가 이메일 인증 코드</h1>
-                <h2>%s</h2>
-                <p>3분 안에 인증을 진행하세요.</p>
-            """;
+    private final String CODE_VARIABLE_NAME = "code";
+    private final String EMAIL_VERIFICATION_CODE_TEMPLATE_NAME = "email-verification-code-template";
+    private final String LOGO_NAME = "logo";
+    private final String LOGO_LOCATION = "static/images/logo.png";
     
     public void send(String email, String code) {
-        javaEmailSender.send(email, SUBJECT, CONTENT.formatted(code));
+        Context context = new Context();
+        context.setVariable(CODE_VARIABLE_NAME, code);
+        
+        String html = templateEngine.process(EMAIL_VERIFICATION_CODE_TEMPLATE_NAME, context);
+        Content content = new Content(LOGO_NAME, LOGO_LOCATION);
+        
+        javaEmailSender.send(email, SUBJECT, html, content);
     }
 }
