86bf6990af6e2a19d802b1d9dd48b92e33dc5910
hky035 <rldud035@naver.com>
2025-12-29 22:07:17 +0900

    feat: 비밀번호 초기화 요청 후 이벤트 발급 및 이메일 발송 리스너 도입
     * 비밀번호 초기화 요청 이벤트 클래스(PasswordResetEvent) 정의
     * 비밀번호 초기화 요청 이벤트 기반 이메일 발송 리스너 클래스(PasswordResetEventEmailListener) 정의
     * 비밀번호 초기화 요청 로직 변경
       * 기존: 주 비즈니스 로직 내 이메일 발송 로직 포함
       * 변경: 비밀번호 초기화 요청 이벤트 발행
       * 트랜잭션 범위에 따른 이벤트 제어를 위한 @Transactional 어노테이션 정의

---
+++ src/main/java/kr/co/yeogiga/application/auth/event/listener/PasswordResetEventEmailListener.java
@@ -0,0 +1,26 @@
+package kr.co.yeogiga.application.auth.event.listener;
+
+import kr.co.yeogiga.application.auth.event.PasswordResetEvent;
+import kr.co.yeogiga.application.event.listener.DomainEventListener;
+import kr.co.yeogiga.infrastructure.mail.PasswordResetEmailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static kr.co.yeogiga.infrastructure.config.AsyncConfig.EMAIL_TASK_EXECUTOR;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordResetEventEmailListener extends DomainEventListener<PasswordResetEvent> {
+    private final PasswordResetEmailSender passwordResetEmailSender;
+    
+    @Override
+    @Async(value = EMAIL_TASK_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleEvent(PasswordResetEvent event) {
+        passwordResetEmailSender.send(event.getEmail(), event.getCode());
+        // TODO: Transactional Outbox Pattern 적용 시, 이벤트 상태 변경 로직 추가 필요
+    }
+}
