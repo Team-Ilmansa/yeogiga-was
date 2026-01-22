12a8f75603d18f1fc401c284e7adf1ea7e087e8e
hky035 <rldud035@naver.com>
2026-01-22 23:31:08 +0900

    feat: DomainEventListener 인터페이스 정의

--- src/main/java/kr/co/yeogiga/application/event/listener/DomainEventListener.java
+++ src/main/java/kr/co/yeogiga/application/event/listener/DomainEventListener.java
@@ -2,6 +2,6 @@ package kr.co.yeogiga.application.event.listener;
 
 import kr.co.yeogiga.domain.event.DomainEvent;
 
-public abstract class DomainEventListener <T extends DomainEvent> {
-    public abstract void handleEvent(T event);
+public interface DomainEventListener {
+    void handleEvent(DomainEvent event);
 }
