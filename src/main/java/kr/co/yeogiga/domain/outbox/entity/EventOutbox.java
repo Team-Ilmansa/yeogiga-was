a85c41fd2378f87d77e10866a11bc569108a339f
hky035 <rldud035@naver.com>
2026-01-23 20:40:03 +0900

    style: 들여쓰기 수정 및 코드 일관성을 위한 어노테이션 수정

--- src/main/java/kr/co/yeogiga/domain/outbox/entity/EventOutbox.java
+++ src/main/java/kr/co/yeogiga/domain/outbox/entity/EventOutbox.java
@@ -7,9 +7,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import kr.co.yeogiga.domain.event.DomainEvent;
 import kr.co.yeogiga.domain.outbox.type.EventOutboxStatus;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +17,8 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Getter
-@Entity
-@Table(name = "event_outbox")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "event_outbox")
 public class EventOutbox {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,7 +46,7 @@ public class EventOutbox {
     @Column(name = "fail_count")
     private int failCount;
     
-        @Builder
+    @Builder
     public EventOutbox(String eventId, String eventType, String payload, LocalDateTime createdAt) {
         this.eventId = eventId;
         this.eventType = eventType;
