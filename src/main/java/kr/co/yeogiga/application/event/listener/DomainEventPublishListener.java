4915e1628f965cbde1fc18752f323f72643fa714
hky035 <rldud035@naver.com>
2026-01-25 18:06:43 +0900

    feat: DomainEventPublishListener 클래스 정의
     * 비동기 로직 내 별도의 트랜잭션을 유지하여 더티 체킹을 통한 event_outbox 엔티티 상태 업데이트

---
+++ src/main/java/kr/co/yeogiga/application/event/listener/DomainEventPublishListener.java
@@ -0,0 +1,36 @@
+package kr.co.yeogiga.application.event.listener;
+
+import kr.co.yeogiga.domain.event.DomainEvent;
+import kr.co.yeogiga.domain.outbox.service.EventOutboxService;
+import kr.co.yeogiga.infrastructure.event.publisher.DomainEventExternalPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.concurrent.Executor;
+
+import static kr.co.yeogiga.infrastructure.config.AsyncConfig.MESSAGE_PUBLISH_TASK_EXECUTOR;
+
+@Component
+@RequiredArgsConstructor
+public class DomainEventPublishListener implements DomainEventListener {
+    private final DomainEventExternalPublisher eventExternalPublisher;
+    private final EventOutboxService eventOutboxService;
+    private final Executor messagePublishTaskExecutor;
+    
+    @Override
+    @Async(value = MESSAGE_PUBLISH_TASK_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleEvent(DomainEvent event) {
+        eventExternalPublisher.publish(event)
+                .thenAcceptAsync(result -> {
+                    if (result.isSuccess()) {
+                        eventOutboxService.updateToDoneByEventId(result.eventId());
+                    } else {
+                        eventOutboxService.updateToFailedByEventId(result.eventId());
+                    }
+                }, messagePublishTaskExecutor);
+    }
+}
