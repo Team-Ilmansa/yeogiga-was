b5c39e1507351cabe71ffb08471468970c881f1a
hky035 <rldud035@naver.com>
2026-01-25 18:00:55 +0900

    feat: 외부 메시지브로커로의 메시지 발행용 ThreadPoolTaskExecutor 정의

--- src/main/java/kr/co/yeogiga/infrastructure/config/AsyncConfig.java
+++ src/main/java/kr/co/yeogiga/infrastructure/config/AsyncConfig.java
@@ -6,11 +6,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
     public final static String EMAIL_TASK_EXECUTOR = "emailTaskExecutor";
+    public final static String MESSAGE_PUBLISH_TASK_EXECUTOR = "messagePublishTaskExecutor";
     
     /**
      * 이메일 발송 작업 수행을 위한 ThreadPoolTaskExecutor 빈
@@ -35,6 +37,32 @@ public class AsyncConfig {
         
         executor.setThreadNamePrefix("email-exec-");
         
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        
+        return executor;
+    }
+    
+    /**
+     * 이벤트 메시지 발행을 위한 ThreadPoolTaskExecutor 빈
+     *
+     * @return 비동기 이메일 발송 작업 용 ThreadPoolTaskExecutor
+     */
+    @Bean(name = MESSAGE_PUBLISH_TASK_EXECUTOR)
+    public Executor messagePublishTaskExecutor() {
+        int cores = Runtime.getRuntime().availableProcessors();
+        int corePoolSize = cores * 2;
+        int maxPoolSize = cores * 4;
+        
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(100);
+        
+        executor.setThreadNamePrefix("message-publish-exec-");
+        
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setAwaitTerminationSeconds(60);
         executor.initialize();
