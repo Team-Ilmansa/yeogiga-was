1771071f5ea513dd786d806342cf1cb9aec5cd5f
hky035 <rldud035@naver.com>
2026-01-29 18:33:20 +0900

    feat: EventOutboxPoller 클래스 구현

--- src/main/java/kr/co/yeogiga/domain/outbox/service/EventOutboxService.java
+++ src/main/java/kr/co/yeogiga/domain/outbox/service/EventOutboxService.java
@@ -2,26 +2,60 @@ package kr.co.yeogiga.domain.outbox.service;
 
 import kr.co.yeogiga.domain.outbox.entity.EventOutbox;
 import kr.co.yeogiga.domain.outbox.repository.EventOutboxRepository;
+import kr.co.yeogiga.domain.outbox.type.EventOutboxStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class EventOutboxService {
     private final EventOutboxRepository eventOutboxRepository;
     
+    private final int MAX_FAIL_COUNT = 3;
+    private final int WAITING_EVENT_MIN_AGE_SECONDS = 30;
+    private final int WAITING_EVENT_MAX_AGE_MINUTES = 3;
+    
     public EventOutbox save(EventOutbox eventOutbox) {
         return eventOutboxRepository.save(eventOutbox);
     }
     
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void updateToDoneByEventId(String eventId) {
+    public List<EventOutbox> readAllPollingEventOutbox() {
+        List<EventOutbox> waitingEventOutboxes = eventOutboxRepository.findByStatusAndCreatedAt(
+                EventOutboxStatus.WAITING,
+                LocalDateTime.now().minusMinutes(WAITING_EVENT_MAX_AGE_MINUTES),
+                LocalDateTime.now().minusSeconds(WAITING_EVENT_MIN_AGE_SECONDS)
+        );
+        
+        List<EventOutbox> failedEventOutboxes = eventOutboxRepository.findByStatusAndFailCount(
+                EventOutboxStatus.FAILED,
+                MAX_FAIL_COUNT
+        );
+        
+        return Stream.concat(waitingEventOutboxes.stream(), failedEventOutboxes.stream()).collect(Collectors.toList());
+    }
+    
+    @Transactional
+    public void updateToPublishedByEventIds(List<String> eventIds) {
+        eventOutboxRepository.updateStatusPublishedInEventIds(eventIds);
+    }
+    
+    @Transactional
+    public void updateToFailedByEventsIds(List<String> eventIds) {
+        eventOutboxRepository.updateStatusFailedInEventIds(eventIds);
+    }
+    
+    @Transactional
+    public void updateToPublishedByEventId(String eventId) {
         eventOutboxRepository.updateStatusPublishedByEventId(eventId);
     }
     
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     public void updateToFailedByEventId(String eventId) {
         eventOutboxRepository.updateStatusFailedByEventId(eventId);
     }
