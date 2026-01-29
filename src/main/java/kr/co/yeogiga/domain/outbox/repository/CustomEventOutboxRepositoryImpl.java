9bc3871bb0343e8088bb20c2492514328db4d3e3
hky035 <rldud035@naver.com>
2026-01-29 17:40:02 +0900

    feat: EventOutbox 조회 및 상대 벌크 업데이트를 위한 QueryDSL 메서드 정의

---
+++ src/main/java/kr/co/yeogiga/domain/outbox/repository/CustomEventOutboxRepositoryImpl.java
@@ -0,0 +1,71 @@
+package kr.co.yeogiga.domain.outbox.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.yeogiga.domain.outbox.entity.EventOutbox;
+import kr.co.yeogiga.domain.outbox.entity.QEventOutbox;
+import kr.co.yeogiga.domain.outbox.type.EventOutboxStatus;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomEventOutboxRepositoryImpl implements CustomEventOutboxRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+    
+    private final QEventOutbox eventOutbox = QEventOutbox.eventOutbox;
+    
+    @Override
+    public List<EventOutbox> findByStatusAndCreatedAt(EventOutboxStatus status, LocalDateTime start, LocalDateTime end) {
+        return jpaQueryFactory
+                .selectFrom(eventOutbox)
+                .where(
+                        eventOutbox.status.eq(status),
+                        createdAtBetween(start, end)
+                )
+                .fetch();
+    }
+    
+    @Override
+    public List<EventOutbox> findByStatusAndFailCount(EventOutboxStatus status, int failCount) {
+        return jpaQueryFactory
+                .selectFrom(eventOutbox)
+                .where(
+                        eventOutbox.status.eq(status),
+                        eventOutbox.failCount.lt(failCount)
+                )
+                .fetch();
+    }
+    
+    @Override
+    public void updateStatusPublishedInEventIds(List<String> eventIds) {
+        if (eventIds.isEmpty()) return;
+        
+        jpaQueryFactory
+                .update(eventOutbox)
+                .set(eventOutbox.status, EventOutboxStatus.PUBLISHED)
+                .where(eventOutbox.eventId.in(eventIds))
+                .execute();
+    }
+    
+    @Override
+    public void updateStatusFailedInEventIds(List<String> eventIds) {
+        if (eventIds.isEmpty()) return;
+        
+        jpaQueryFactory
+                .update(eventOutbox)
+                .set(eventOutbox.status, EventOutboxStatus.FAILED)
+                .set(eventOutbox.lastRetriedAt, LocalDateTime.now())
+                .where(eventOutbox.eventId.in(eventIds))
+                .execute();
+    }
+    
+    
+    private BooleanExpression createdAtBetween(LocalDateTime start, LocalDateTime end) {
+        if (start == null && end == null) return null;
+        if (start == null) return eventOutbox.createdAt.loe(end);
+        if (end == null) return eventOutbox.createdAt.goe(start);
+        return eventOutbox.createdAt.between(start, end);
+    }
+}
--- src/main/java/kr/co/yeogiga/domain/outbox/repository/EventOutboxRepository.java
+++ src/main/java/kr/co/yeogiga/domain/outbox/repository/EventOutboxRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface EventOutboxRepository extends JpaRepository<EventOutbox, Long> {
+public interface EventOutboxRepository extends JpaRepository<EventOutbox, Long>, CustomEventOutboxRepository {
     @Modifying
     @Query(value = "UPDATE event_outbox " +
                    "SET status = 'PUBLISHED' " +
