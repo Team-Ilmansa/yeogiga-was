079ca48e752f4ed157f6924c2f95d02bad22228f
hky035 <rldud035@naver.com>
2026-02-04 17:48:44 +0900

    feat: 스케줄링 방식 별 사용 쓰레드풀 구분 설정
     * `@Scheduled`
     * taskScheduler.schedule() 메서드를 통한 직접 등록

--- src/main/java/kr/co/yeogiga/infrastructure/config/SchedulingConfig.java
+++ src/main/java/kr/co/yeogiga/infrastructure/config/SchedulingConfig.java
@@ -1,9 +1,28 @@
 package kr.co.yeogiga.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
 public class SchedulingConfig {
+    @Bean(name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(4);
+        taskScheduler.setThreadNamePrefix("general-scheduling-");
+        return taskScheduler;
+    }
+    
+    @Bean(name = "eventCleanupTaskScheduler")
+    public ThreadPoolTaskScheduler eventCleanupTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(4);
+        taskScheduler.setThreadNamePrefix("event-cleanup-scheduling-");
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        taskScheduler.setAwaitTerminationSeconds(60);
+        return taskScheduler;
+    }
 }
